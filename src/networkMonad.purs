22051902e643858428af34ec76806e8164f4aaf2
Joop Ringelberg <joop.ringelberg@gmail.com>
2017-07-03 12:06:13 +0200

    Added type 'Update'
    A type alias for the second parameter of the function bindAndUpdate of
    the NetworkMonad has been established.

--- src/networkMonad.purs
+++ src/networkMonad.purs
@@ -11,9 +11,15 @@ module Perspectives.NetworkMonad where
 import Control.Monad
 import Delta (Delta(..))
 
+-- | Notice the extra parameter of type Update, a function that will produce an Array of Deltas to be performed on m b.
+-- | Notice, too, that the Delta is in terms of the type that is wrapped in Monad m. E.g., if a is Array x, we have
+-- | Delta Array x and not Delta a!
 class Monad m <= NetworkMonad m where
 	bindAndUpdate :: forall a b. m a -> (a -> m b) -> (Delta a -> b -> b) -> m b
 
+type Update a b = Delta a -> b -> b
+
+{-}
 -- | Produces an Update function from a function from a to b.
 singleValueDelta :: forall a b. (a -> b) -> Delta a -> b -> Array Delta b
 singleValueDelta f (Erbij a) payload = [Erbij (f a)]
@@ -29,3 +35,4 @@ arrayBindDelta f (Eraf [a]) payload = first <<< map (\d -> removeFrom d) (f a)
 
 foreign import nettoAddTo :: Array a -> a -> Array a
 foreign import removeFrom :: Array a -> a -> Array a
+-}
