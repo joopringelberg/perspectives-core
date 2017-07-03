a9a8687024cfdd2fba892fd9b98fe56f09c2e010
Joop Ringelberg <joop.ringelberg@gmail.com>
2017-07-03 21:25:18 +0200

    Type based network
    A network implementation with type and newtype.

---
+++ src/testNet.purs
@@ -0,0 +1,43 @@
+-- | A test with a network structure
+
+module Test.Network where
+
+import Perspectives.DestructiveArray
+import Perspectives.DestructiveMap (DestructiveMap, setInMap, getFromMap, deleteFromMap)
+
+type Name = String
+
+type Edge =
+  { name :: String
+  , target :: Node
+  }
+
+type EdgeMap = DestructiveMap Edge
+type Supports = DestructiveArray Node
+
+newtype Node = Node
+  { name :: String
+  , dependents :: EdgeMap
+  , supports :: Supports
+  }
+
+--getNodeName :: forall a. Node a -> Name
+getNodeName :: forall r. { name :: String | r } -> Name
+getNodeName {name} = name
+
+
+getDependent :: Node -> Name -> Edge
+getDependent (Node { dependents }) k = getFromMap dependents k
+
+addDependent :: Node -> Name -> Edge -> EdgeMap
+addDependent	source@(Node { dependents }) name edge@{ target : Node({ supports }) }
+  = setInMap dependents name edge where
+      discard = pushInDestructiveArray supports source
+
+removeDependent :: Node -> Name -> EdgeMap
+removeDependent	source@( Node { dependents }) name
+  = let
+        edge = getDependent source name
+        discard = deleteFromMap dependents name
+    in
+      deleteFromMap dependents name
