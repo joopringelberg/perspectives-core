6f0a4352ceab2e682a74da2b7fa81a7c848f28c2
Joop Ringelberg <joop.ringelberg@gmail.com>
2017-08-24 16:12:48 +0200

    The property combinators no longer return duplicates

--- src/propertyComposition.purs
+++ src/propertyComposition.purs
@@ -1,25 +1,21 @@
 module Perspectives.PropertyComposition where
 
+import Control.Bind ((>=>))
 import Control.Monad.Aff (Aff)
 import Data.Array (cons, foldr, nub)
 import Data.Eq (class Eq)
 import Data.Maybe (Maybe(..), maybe)
-import Data.Traversable (class Traversable, traverse)
-import Prelude (class Bind, bind, id, join, pure, ($))
+import Data.Traversable (traverse)
+import Prelude (bind, id, join, pure, ($))
 
--- | Duplicates must be removed.
-xTox :: forall a b c trav e. Traversable trav => Bind trav =>
-  (a -> Aff e (trav b))
-  -> (b -> Aff e (trav c))
+sTos :: forall a b c e.
+  (a -> Aff e (Maybe b))
+  -> (b -> Aff e (Maybe c))
   -> a
-  -> Aff e (trav c)
-xTox f g a = do
-  x <- f a
-  y <- traverse g x
-  pure (join y)
--- xTox f g = f >=> traverse g >=> (pure <<< join)
+  -> Aff e (Maybe c)
+sTos f g = f >=> (maybe (pure Nothing) g)
 
-infix 0 xTox as >->
+infix 0 sTos as >->
 
 sTop :: forall a b c e.
   (a -> Aff e (Maybe b))
@@ -28,15 +24,13 @@ sTop :: forall a b c e.
   -> Aff e (Array c)
 sTop f g a = do
   x <- f a
-  -- x :: Maybe b
-  case x of
+  case (x :: Maybe b) of
     Nothing -> pure []
     (Just b) -> g b
 -- sTop f g = f >=> (maybe (pure []) g)
 
 infix 0 sTop as >->>
 
--- | Duplicates must be removed.
 pTos :: forall a b c e. Eq c =>
   (a -> Aff e (Array b))
   -> (b -> Aff e (Maybe c))
@@ -44,16 +38,20 @@ pTos :: forall a b c e. Eq c =>
   -> Aff e (Array c)
 pTos f g a = do
   x <- f a
-  -- x :: Array b
-  y <- traverse g x
-  -- y :: Array (Maybe c)
-  pure $ nub $ foldr (maybe id cons) [] y
+  y <- traverse g (x :: Array b)
+  pure $ nub $ foldr (maybe id cons) [] (y :: Array (Maybe c))
 
 infix 0 pTos as >>->
 
--- traverse :: forall a b m. Applicative m => (a -> m b) -> t a -> m (t b)
--- a = b
--- m = Aff e
--- b = Maybe c
--- t = Array
--- traverse :: (b -> Aff e (Maybe c)) -> Array b -> Aff e (Array (Maybe c))
+pTop :: forall a b c e. Eq c =>
+  (a -> Aff e (Array b))
+  -> (b -> Aff e (Array c))
+  -> a
+  -> Aff e (Array c)
+pTop f g a = do
+  x <- f a
+  y <- traverse g (x :: Array b)
+  pure $ nub (join (y :: Array (Array c)))
+-- xTox f g = f >=> traverse g >=> (pure <<< nub <<< join)
+
+infix 0 pTop as >>->>
