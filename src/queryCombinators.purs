626f017305630c98836c1681868fb8c5e332d791
Joop Ringelberg <joop.ringelberg@gmail.com>
2017-08-24 17:31:43 +0200

    aClosure works. Added concat, cons and identity

--- src/queryCombinators.purs
+++ src/queryCombinators.purs
@@ -1,12 +1,13 @@
 module Perspectives.QueryCombinators where
 
 import Prelude
-import Data.Array (cons, elemIndex, nub, union)
+import Data.Array (cons, elemIndex, nub, union) as Arr
 import Data.Maybe (Maybe(..))
 import Data.Traversable (traverse)
 import Perspectives.Property (AsyncPropDefs, PluralGetter, SingleGetter)
 import Perspectives.ResourceTypes (Resource)
 
+-- | Compute the transitive closure of the SingleGetter to obtain a PluralGetter. NB: only for Resource results!
 mclosure :: SingleGetter Resource -> PluralGetter Resource
 mclosure f rs = closure' rs [] where
   closure' :: forall e. Resource -> Array Resource -> AsyncPropDefs e (Array Resource)
@@ -15,20 +16,37 @@ mclosure f rs = closure' rs [] where
     case t of
       Nothing -> pure ts
       (Just x) ->
-        case elemIndex (x :: Resource) ts of
-          Nothing -> closure' x (cons x ts)
+        case Arr.elemIndex (x :: Resource) ts of
+          Nothing -> closure' x (Arr.cons x ts)
           _ -> pure ts
 
+-- | Compute the transitive closure of the PluralGetter to obtain a PluralGetter. NB: only for Resource results!
 aclosure :: PluralGetter Resource -> PluralGetter Resource
 aclosure f r = do
   (t :: Array Resource) <- f r
   (x :: Array (Array Resource)) <- traverse (aclosure f) t
   (y :: Array Resource) <- pure $ join x
-  pure $ nub (union t y)
+  pure $ Arr.nub (Arr.union t y)
 
---traverse :: forall a b m. Applicative m => (a -> m b) -> t a -> m (t b)
--- a = Resource
--- m = AsyncPropDefs e
--- b = Array Resource
--- t = Array
--- Resource -> AsyncPropDefs (Array Resource)) -> Array Resource -> AsyncPropDefs e (Array (Array Resource))
+-- | Concatenate the results of two PluralGetters.
+concat :: forall a. Eq a => PluralGetter a -> PluralGetter a -> PluralGetter a
+concat f g r = do
+  a <- f r
+  b <- g r
+  pure $ Arr.union a b
+
+-- | Add the result of a SingleGetter to that of a PluralGetter.
+cons :: forall a. Eq a => SingleGetter a -> PluralGetter a -> PluralGetter a
+cons f g r = do
+  (a :: Maybe a) <- f r
+  (b :: Array a) <- g r
+  case a of
+    Nothing -> pure b
+    (Just el) ->
+      case Arr.elemIndex el b of
+        Nothing -> pure $ Arr.cons el b
+        _ -> pure b
+
+-- | Identity function: from a Resource a, compute Aff e (Just a) 
+identity :: SingleGetter Resource
+identity = pure <<< Just
