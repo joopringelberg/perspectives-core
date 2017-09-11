2563c262b24e7bed753d5b8b33755052cbb4ef36
Joop Ringelberg <joop.ringelberg@gmail.com>
2017-09-11 10:10:49 +0200

    GLStrMap as resourceIndex

--- src/resource.purs
+++ src/resource.purs
@@ -5,51 +5,50 @@ import Control.Monad.Aff (Aff)
 import Control.Monad.Aff.AVar (AVar, makeVar', peekVar, AVAR)
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Class (liftEff)
-import Control.Monad.ST (ST)
 import Data.Maybe (Maybe(..))
-import Data.StrMap (StrMap, empty, lookup)
-import Data.StrMap.ST (poke, STStrMap)
+-- import Data.StrMap.ST (poke, STStrMap)
+import Perspectives.GlobalUnsafeStrMap (GLOBALMAP, GLStrMap, new, poke, peek)
 import Partial.Unsafe (unsafePartial)
 import Perspectives.Location (Location, THEORYDELTA, locate, setLocationValue)
 import Perspectives.ResourceRetrieval (fetchPropDefs)
 import Perspectives.ResourceTypes (PropDefs, Resource(..), ResourceId, ResourceLocation(..), AsyncDomeinFile)
 
 -- | The global index of all Resource-Location pairs, indexed by ResourceId.
-type ResourceIndex = StrMap ResourceLocation
+type ResourceIndex = GLStrMap ResourceLocation
+
 resourceIndex :: ResourceIndex
-resourceIndex = empty
+resourceIndex = new unit
 
 -- | From a Resource, find its Location.
 resourceLocation :: Resource -> Location Resource
 resourceLocation (Resource{id}) = unsafePartial resourceLocation' id where
     resourceLocation' :: Partial => String -> Location Resource
-    resourceLocation' rid = case lookup id resourceIndex of
+    resourceLocation' rid = case peek resourceIndex id of
       (Just (ResourceLocation{ loc })) -> loc
 
 -- | Look up a resource or create a new resource without definition and store it in the index.
-representResource :: forall e. ResourceId -> Eff (st :: ST ResourceIndex | e) Resource
-representResource id = case lookup id resourceIndex of
+representResource :: forall e. ResourceId -> Eff (gm :: GLOBALMAP | e) Resource
+representResource id = case peek resourceIndex id of
   Nothing -> storeResourceInIndex $ Resource{ id: id, propDefs: Nothing}
   (Just (ResourceLocation {res})) -> pure res
 
 -- | Create a new resource with definitions and store it in the index.
-newResource :: ResourceId -> PropDefs -> Aff ( st :: ST ResourceIndex, avar :: AVAR ) Resource
+newResource :: ResourceId -> PropDefs -> Aff ( gm :: GLOBALMAP, avar :: AVAR ) Resource
 newResource id defs = do
     v <- makeVar' defs
     res <- pure (Resource{ id: id, propDefs: Just v})
     liftEff $ storeResourceInIndex res
 
-storeResourceInIndex :: forall e. Resource -> Eff (st :: ST ResourceIndex | e) Resource
+storeResourceInIndex :: forall e. Resource -> Eff (gm :: GLOBALMAP | e) Resource
 storeResourceInIndex res@(Resource{id}) =
   let loc = locate res
   in do
-    ri <- thawST' resourceIndex
-    poke ri id (ResourceLocation{ res: res, loc: loc}) *> pure res
+    poke resourceIndex id (ResourceLocation{ res: res, loc: loc}) *> pure res
 
 -- | A version of thawST that does not copy.
-foreign import thawST' :: forall a h r. StrMap a -> Eff (st :: ST h | r) (STStrMap h a)
+-- foreign import thawST' :: forall a h r. StrMap a -> Eff (st :: ST h | r) (STStrMap h a)
 
-addPropertyDefinitions :: forall e. Resource -> AVar PropDefs -> Eff (td :: THEORYDELTA, st :: ST ResourceIndex | e) Unit
+addPropertyDefinitions :: forall e. Resource -> AVar PropDefs -> Eff (td :: THEORYDELTA, gm :: GLOBALMAP | e) Unit
 addPropertyDefinitions r@(Resource{id}) av =
   let
     loc = resourceLocation r
@@ -57,8 +56,7 @@ addPropertyDefinitions r@(Resource{id}) av =
     newResourceLocation = ResourceLocation{ res: r, loc: loc }
   in do
     _ <- setLocationValue loc newR
-    ri <- thawST' resourceIndex
-    _ <- poke ri id newResourceLocation
+    _ <- poke resourceIndex id newResourceLocation
     pure unit
 
 -- | Get the property definitions of a Resource.
