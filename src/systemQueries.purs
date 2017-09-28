333a6cb4b5578f3908a4a3f18b0faf7606a9ecde
Joop Ringelberg <joop.ringelberg@gmail.com>
2017-09-28 13:59:57 +0200

    SystemQueries are now in StackedLocation

--- src/systemQueries.purs
+++ src/systemQueries.purs
@@ -3,8 +3,8 @@ module Perspectives.SystemQueries where
 import Perspectives.PropertyComposition
 import Perspectives.QueryCombinators as QC
 import Data.Maybe (Maybe(..))
-import Perspectives.Location (nameFunction, nestLocationInMonad)
-import Perspectives.Property (MemorizingPluralGetter, MemorizingSingleGetter, SingleGetter, getResource, getResources, getString)
+import Perspectives.Location (nameFunction)
+import Perspectives.Property (SingleGetter, StackedMemorizingSingleGetter, StackedMemorizingPluralGetter, getResource, getResources, getString)
 import Perspectives.ResourceTypes (Resource(..))
 import Prelude (pure)
 
@@ -12,35 +12,35 @@ identifier' :: SingleGetter String
 identifier' Nothing = pure Nothing
 identifier' (Just (Resource {id})) = pure (Just id)
 
-identifier :: MemorizingSingleGetter String
-identifier = nestLocationInMonad (nameFunction "identifier" identifier')
+identifier :: StackedMemorizingSingleGetter String
+identifier = memorizeInStackedLocation (nameFunction "identifier" identifier')
 
-label :: MemorizingSingleGetter String
-label = nestLocationInMonad (getString "rdfs:label")
+label :: StackedMemorizingSingleGetter String
+label = memorizeInStackedLocation (getString "rdfs:label")
 
-subClassOf :: MemorizingPluralGetter Resource
-subClassOf = nestLocationInMonad (getResources "rdfs:subClassOf")
+subClassOf :: StackedMemorizingPluralGetter Resource
+subClassOf = memorizeInStackedLocation (getResources "rdfs:subClassOf")
 
-rdfType :: MemorizingSingleGetter Resource
-rdfType = nestLocationInMonad (getResource "rdf:type")
+rdfType :: StackedMemorizingSingleGetter Resource
+rdfType = memorizeInStackedLocation (getResource "rdf:type")
 
-rol_RolBinding :: MemorizingSingleGetter Resource
-rol_RolBinding = nestLocationInMonad (getResource "model:SysteemDomein#rol_RolBinding")
+rol_RolBinding :: StackedMemorizingSingleGetter Resource
+rol_RolBinding = memorizeInStackedLocation (getResource "model:SysteemDomein#rol_RolBinding")
 
 -- | NB. Dit is onvoldoende. Alleen de 'buitenste' aanroep wordt gememoiseerd; niet de recursieve.
-types :: MemorizingPluralGetter Resource
+types :: StackedMemorizingPluralGetter Resource
 types = nameFunction "types" (QC.mclosure rdfType)
 
 -- | NB. Dit is onvoldoende. Alleen de 'buitenste' aanroep wordt gememoiseerd; niet de recursieve.
-superClasses :: MemorizingPluralGetter Resource
+superClasses :: StackedMemorizingPluralGetter Resource
 superClasses = nameFunction "superClasses" (QC.aclosure subClassOf)
 
-typeSuperClasses :: MemorizingPluralGetter Resource
+typeSuperClasses :: StackedMemorizingPluralGetter Resource
 typeSuperClasses = nameFunction "typeSuperClasses" (QC.addTo rdfType (rdfType >->> superClasses))
 -- -- typeSuperClasses = nameFunction "typeSuperClasses" (rdfType >->> QC.addTo QC.identity superClasses)
 
-hasLabel :: MemorizingSingleGetter Boolean
+hasLabel :: StackedMemorizingSingleGetter Boolean
 hasLabel = QC.hasValue label
 
-hasBinding :: MemorizingSingleGetter Boolean
+hasBinding :: StackedMemorizingSingleGetter Boolean
 hasBinding = QC.hasValue rol_RolBinding
