6ffb159c348273b28805bc16cc6f5da6de55bafd
Joop Ringelberg <joop.ringelberg@gmail.com>
2017-10-03 16:20:09 +0200

    Testing

--- test/TestProperties.purs
+++ test/TestProperties.purs
@@ -22,49 +22,51 @@ test = launchAff $ runLocationT do
   log ( "(rol_RolBinding >-> label) user:xGebruiker = " <> (show l))
 
   log "========================================================="
-  h <-  (rdfType) gbLoc
-  log ( "(rdfType) user:xGebruiker = " <> (show h))
+  h <-  rdfType gbLoc
+  log ( "rdfType user:xGebruiker = " <> (show h))
 
   log "========================================================="
   h' <-  types gbLoc
-  log ( "(types user:xGebruiker = " <> (show h'))
+  log ( "types user:xGebruiker = " <> (show h'))
 
   log "========================================================="
   ekdLoc <- liftEff $ representResource "model:ExecutieKetenDomein#ExecutieKetenDomein"
-  m <-  (label) ekdLoc
+  m <-  label ekdLoc
   log ( "label model:ExecutieKetenDomein#ExecutieKetenDomein = " <> (show m))
 
 
   log "========================================================="
-  n <-  (subClassOf >>-> label) ekdLoc
-  log ( "(subClassOf >>-> label) model:ExecutieKetenDomein#ExecutieKetenDomein = " <> (show n))
+  n <-  subClassOf >>-> label $ ekdLoc
+  log ( "subClassOf >>-> label $ model:ExecutieKetenDomein#ExecutieKetenDomein = " <> (show n))
 
   log "========================================================="
-  o <-  (rdfType >-> label) gbLoc
-  log ( "(rdfType >-> label) user:xGebruiker = " <> (show o))
+  o <-  rdfType >-> label $ gbLoc
+  log ( "rdfType >-> label $ user:xGebruiker = " <> (show o))
 
   log "========================================================="
-  p <-  (types >>-> label) gbLoc
-  log ( "(types >>-> label user:xGebruiker) = " <> (show p))
+  p <-  types >>-> label $ gbLoc
+  log ( "types >>-> label user:xGebruiker = " <> (show p))
 
   log "========================================================="
-  p' <-  (rol_RolBinding >-> rdfType >->> types >>->> subClassOf >>-> label) gbLoc
-  log ( "(( rol_RolBinding >-> rdfType >->> types >>->> subClassOf >>-> label) user:xGebruiker) = " <> (show p'))
+  p' <-  rol_RolBinding >-> rdfType >->> types >>->> subClassOf >>-> label $ gbLoc
+  log ( "rol_RolBinding >-> rdfType >->> types >>->> subClassOf >>-> label $ user:xGebruiker = " <> (show p'))
 
   log "========================================================="
-  q <-  (rdfType >->> superClasses) gbLoc
-  log ( "(rdfType >->> superClasses) user:xGebruiker = " <> (show q))
+  q <-  rdfType >->> superClasses $ gbLoc
+  log ( "rdfType >->> superClasses $ user:xGebruiker = " <> (show q))
 
   log "========================================================="
-  q <-  (typeSuperClasses >>-> identifier) gbLoc
-  log ( "(typeSuperClasses >>-> identifier) user:xGebruiker = " <> (show q))
+  q' <-  typeSuperClasses >>-> identifier $ gbLoc
+  log ( "typeSuperClasses >>-> identifier $ user:xGebruiker = " <> (show q'))
+  --
+  log "========================================================="
+  r <-  typeSuperClasses >>-> hasLabel $ gbLoc
+  log ( "typeSuperClasses >>-> hasLabel $ user:xGebruiker = " <> (show r))
 
   log "========================================================="
-  r <-  (typeSuperClasses >>-> hasLabel) gbLoc
-  log ( "(typeSuperClasses >>-> hasLabel) user:xGebruiker = " <> (show r))
-
-  log "========================================================="
-  s <-  (QC.filter hasLabel typeSuperClasses  >>-> identifier) gbLoc
-  log ( "(filter hasLabel typeSuperClasses) user:xGebruiker = " <> (show s))
+  -- s <-  (QC.filter hasLabel typeSuperClasses) gbLoc
+  -- log ( "(QC.filter hasLabel typeSuperClasses) user:xGebruiker = " <> (show s))
+  s' <-  (QC.filter hasLabel "onlyWithLabel" typeSuperClasses) >>-> identifier $ gbLoc
+  log ( "(QC.filter hasLabel typeSuperClasses) >>-> identifier $ user:xGebruiker = " <> (show s'))
 
 log = lift <<< Aff.log
---
+++ test/testConnections.purs
@@ -0,0 +1,78 @@
+module Test.Connections where
+
+import Prelude
+import Perspectives.PropertyComposition
+import Perspectives.SystemQueries
+import Control.Monad.Aff (launchAff)
+import Control.Monad.Aff.Console (log) as Aff
+import Control.Monad.Eff.Class (liftEff)
+import Control.Monad.Trans.Class (lift)
+import Data.Maybe (Maybe(..))
+import Perspectives.Location (Location, saveInLocation, locationValue)
+import Perspectives.LocationT (runLocationT)
+import Perspectives.Property (MemorizingSingleGetter, SingleGetter)
+import Perspectives.QueryCombinators (identity)
+import Perspectives.QueryCombinators (filter) as QC
+import Perspectives.Resource (locationFromMaybeResource, representResource)
+import Perspectives.ResourceTypes (Resource)
+
+test = launchAff $ runLocationT do
+  -- (gb :: Maybe Resource) <- liftEff $ representResource "user:xGebruiker"
+  -- (gebruikerType :: Maybe Resource) <- rdfType gb
+  -- (labelGebruikerType :: Maybe String) <- label gebruikerType
+  -- (identifierGebruikerType :: Maybe String) <- identifier gebruikerType
+  -- log $ show gb <> ", " <> show gebruikerType <> ", " <> show labelGebruikerType <> ", " <> show identifierGebruikerType
+  -- gtype <- liftEff $ representResource "model:SysteemDomein#Gebruiker"
+  -- _ <- pure $ catchit gtype
+  -- pure gb
+
+  -- (entiteit :: Maybe Resource) <- liftEff $ representResource "model:SysteemDomein#Entiteit"
+  -- entiteitSuperClasses <- (superClasses >>-> label) entiteit
+  -- log (show entiteitSuperClasses)
+  -- _ <- pure $ catchit entiteit
+  -- pure entiteit
+
+  -- (entiteit :: Maybe Resource) <- liftEff $ representResource "model:SysteemDomein#Entiteit"
+  -- entiteitTypes <- (types >>-> label) entiteit
+  -- _ <- pure $ catchit entiteit
+  -- entiteitTypes2 <- (types >>-> label) entiteit
+  -- log (show entiteitTypes)
+  -- _ <- pure $ catchit entiteit
+  -- pure entiteit
+
+  -- (gebruiker :: Maybe Resource) <- liftEff $ representResource "model:SysteemDomein#Gebruiker"
+  -- x <- types >>-> label $ gebruiker
+  -- x' <- subClassOf >>->> types >>-> label $ gebruiker
+  -- _ <- pure $ catchit gebruiker
+  -- y <- types >>-> identifier $ gebruiker
+  -- y' <- subClassOf >>->> types >>-> identifier $ gebruiker
+  -- _ <- pure $ catchit gebruiker
+  -- log $ show x
+  -- log $ show y
+
+  -- (gebruiker :: Maybe Resource) <- liftEff $ representResource "model:SysteemDomein#Gebruiker"
+  -- x <- identity >->> types >>-> label $ gebruiker
+  -- _ <- pure $ catchit gebruiker
+  -- log $ show x
+
+  -- (gebruiker :: Maybe Resource) <- liftEff $ representResource "model:SysteemDomein#Gebruiker"
+  -- x' <- identity >->> (QC.filter hasLabel "onlyWithLabel" types) >>-> identifier $ gebruiker
+  -- log $ show x'
+  -- _ <- pure $ catchit gebruiker
+  -- x <- identity >->> types >>-> identifier $ gebruiker
+  -- log $ show x
+  -- _ <- pure $ catchit gebruiker
+  -- z <- identity >->> (QC.filter hasLabel "onlyWithLabel" types) >>-> identifier $ gebruiker
+  -- log $ show z
+  -- log "Klaar"
+
+  (gebruiker :: Maybe Resource) <- liftEff $ representResource "model:SysteemDomein#Gebruiker"
+  x <- identity >->> superClasses >>-> label $ gebruiker
+  log $ show x
+  _ <- pure $ catchit gebruiker
+  log "Klaar"
+
+log = lift <<< Aff.log
+
+catchit :: forall a. a -> a
+catchit mr = mr
