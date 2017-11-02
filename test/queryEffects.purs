7452d59ff3c4b7777e5d14605e82efe66b4de23a
Joop Ringelberg <joop.ringelberg@gmail.com>
2017-11-02 21:21:02 +0100

    Set value of query result

--- test/queryEffects.purs
+++ test/queryEffects.purs
@@ -7,10 +7,10 @@ import Control.Monad.Eff.Class (liftEff)
 import Control.Monad.Eff.Console (CONSOLE, logShow)
 import Perspectives.PropertyComposition ((>->))
 import Perspectives.QueryEffect ((~>))
-import Perspectives.TheoryChange (setProperty, updateFromSeeds)
+import Perspectives.TheoryChange (saveChangedObject, setObject, setProperty, updateFromSeeds)
 import Perspectives.TripleAdministration (NamedFunction(..), lookupInTripleIndex)
 import Perspectives.TripleGetter (NamedTripleGetter, (##))
-import Prelude (class Show, Unit, discard, void, bind)
+import Prelude (class Show, Unit, bind, discard, flip, void, (>>=))
 import Test.TestEffects (CancelerEffects)
 
 test :: forall e. Aff (CancelerEffects e) Unit
@@ -20,9 +20,12 @@ test = do
   void (liftEff (logShow "======================"))
   t <- setProperty "user:xGebruiker" "rdfs:label" ["Nieuw label voor gebruiker!"]
   t' <- setProperty "user:yNatuurlijkPersoon" "rdfs:label" ["Nieuw label voor NatuurlijkPersoon"]
-  -- void (liftEff (logShow "======================"))
   x <- setProperty "user:xGebruiker" "model:SysteemDomein#rol_RolBinding" ["model:SysteemDomein#Gebruiker"]
   void (updateFromSeeds [t, t', x])
+  void (liftEff (logShow "======================"))
+  y <- ("user:xGebruiker" ## rol_RolBinding >-> label) >>= (flip saveChangedObject ["Hello Pirate!"])
+  y' <- setObject ("user:xGebruiker" ## rol_RolBinding >-> label) ["Hello Pirate!"]
+  void (updateFromSeeds [y])
 
   void (liftEff (lookupInTripleIndex "user:xGebruiker" "rdfs:label"))
 
