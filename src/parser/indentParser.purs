98d69dd0c2c6baa59286a6fbfa9f428609997737
Joop Ringelberg <joop.ringelberg@gmail.com>
2017-12-22 14:28:36 +0100

    State of ContextRoleParser now includes namespace

--- src/parser/indentParser.purs
+++ src/parser/indentParser.purs
@@ -9,29 +9,34 @@ import Data.Either (Either)
 import Data.Maybe (Maybe, maybe)
 import Data.StrMap (StrMap, empty, insert, lookup)
 import Perspectives.Syntax (RoleName)
-import Prelude (Unit, bind, pure, (<<<), (+))
+import Prelude (Unit, bind, pure, (+), (<<<), (>>=))
 import Text.Parsing.Indent (runIndent)
 import Text.Parsing.Parser (ParseError, ParserT, runParserT)
 import Text.Parsing.Parser.Pos (Position)
 
--- | A type to keep track of the number of occurrences of a role type.
-type RoleInstances = StrMap Int
+-- | A type to keep track of:
+-- | - the number of occurrences of a role type, and
+-- | - the namespace for context declarations.
+type RoleInstancesAndNamespace = { rolOccurrences :: StrMap Int, namespace :: String}
+
+initialContextRoleParserMonadState :: RoleInstancesAndNamespace
+initialContextRoleParserMonadState = {rolOccurrences: empty, namespace: "model:Perspectives"}
 
 -- | This is the monad stack we use for the ContextRoleParser.
 -- | The underlying monad is Aff, which we need to access couchdb.
--- | Then we have RoleInstances, which we need to keep track of the number of
+-- | Then we have RoleInstancesAndNamespace, which we need to keep track of the number of
 -- | instances of a particular role type in a context (to generate unique names).
 -- | Finally, the StateT Position part is used by the IndentParser.
-type ContextRoleParserMonad e = (StateT Position (StateT RoleInstances (Aff e)))
+type ContextRoleParserMonad e = (StateT Position (StateT RoleInstancesAndNamespace (Aff e)))
 
 -- | This is the type that is produced by the ContextRoleParser.
 -- | It can also be expressed in terms of the type synonym IndentParser:
--- | type IP a e = IndentParser (StateT RoleInstances (Aff e)) String a
+-- | type IP a e = IndentParser (StateT RoleInstancesAndNamespace (Aff e)) String a
 type IP a e = ParserT String (ContextRoleParserMonad e) a
 
 -- | Apply a parser, keeping only the parsed result.
 runIndentParser :: forall a e. String -> IP a e -> Aff e (Either ParseError a)
-runIndentParser s p = evalStateT (runIndent (runParserT s p)) empty
+runIndentParser s p = evalStateT (runIndent (runParserT s p)) initialContextRoleParserMonadState
 
 -- | As convienience, to lift functions on Aff all the way up:
 liftAffToIP :: forall a e. Aff e a -> IP a e
@@ -41,15 +46,27 @@ liftAffToIP = lift <<< lift <<< lift
 -- | role has been instantiated in a context:
 getRoleOccurrences :: forall e. RoleName -> IP (Maybe Int) e
 getRoleOccurrences roleName = do
-  g <- lift (lift (gets (\rmap -> lookup roleName rmap)))
+  g <- lift (lift (gets (\{rolOccurrences} -> lookup roleName rolOccurrences)))
   pure g
 
 -- | Increment the number of instances of a particular role.
 incrementRoleInstances :: forall e. RoleName -> IP Unit e
-incrementRoleInstances roleName = lift (lift (modify (\rmap -> insert roleName (maybe 1 ((+)1) (lookup roleName rmap)) rmap)))
+incrementRoleInstances roleName = lift (lift (modify f))
+  where
+    f s@{rolOccurrences} = s {rolOccurrences = insert roleName (maybe 1 ((+)1) (lookup roleName rolOccurrences)) rolOccurrences}
 
-getRoleInstances :: forall e. IP RoleInstances e
-getRoleInstances = lift (lift get)
+getRoleInstances :: forall e. IP (StrMap Int) e
+getRoleInstances = lift (lift get) >>= pure <<< (_.rolOccurrences)
 
-setRoleInstances :: forall e. RoleInstances -> IP Unit e
-setRoleInstances rmap = lift (lift (put rmap))
+setRoleInstances :: forall e. (StrMap Int) -> IP Unit e
+setRoleInstances rmap = do
+  s <- lift (lift get)
+  lift (lift (put s {rolOccurrences = rmap}))
+
+getNamespace :: forall e. IP String e
+getNamespace = lift (lift get) >>= pure <<< (_.namespace)
+
+setNamespace :: forall e. String -> IP Unit e
+setNamespace ns = do
+  s <- lift (lift get)
+  lift (lift (put s {namespace = ns}))
