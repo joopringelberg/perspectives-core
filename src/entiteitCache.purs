0c9adbb508078dbcc7f470f6926728787819f1dd
Joop Ringelberg <joop.ringelberg@gmail.com>
2018-02-12 11:01:00 +0100

    Renamed resourceTypes.purs to entiteitCache.purs

--- src/entiteitCache.purs
+++ src/entiteitCache.purs
@@ -1,25 +1,14 @@
-module Perspectives.ResourceTypes
+module Perspectives.EntiteitCache
 
 where
 
 import Control.Monad.Aff.AVar (AVar)
 import Control.Monad.Eff (kind Effect)
-import Data.Argonaut (Json)
-import Data.StrMap (StrMap, keys)
 import Perspectives.GlobalUnsafeStrMap (GLStrMap, new)
 import Perspectives.Syntax (PerspectContext, PerspectRol)
-import Prelude (class Show, show, unit, ($), (<>))
+import Prelude (unit)
 
 type Resource = String
-
--- | A type for an untyped resource taken from Couchdb.
-type CouchdbResource = StrMap Json
-
--- | A newtype for the property definitions so we can show them.
-newtype PropDefs = PropDefs (StrMap Json)
-
-instance showPropDefs :: Show PropDefs where
-  show (PropDefs s) = "PropDefs=" <> (show $ keys s)
 
 type RolDefinitions = GLStrMap (AVar PerspectRol)
 
