ec01a44b173f7163c261aa2c0e71c4a8c355c98c
Joop Ringelberg <joop.ringelberg@gmail.com>
2018-03-07 16:00:16 +0100

    work in progress on user setup

---
+++ src/setupUser.purs
@@ -0,0 +1,56 @@
+module Perspectives.SetupUser where
+
+import Control.Monad.Eff.Now (NOW)
+import Data.Unit (Unit)
+import Perspectives.ContextAndRole (defaultContextRecord, defaultRolRecord)
+import Perspectives.Deltas (addProperty, addRol, runInTransactie)
+import Perspectives.Effects (AjaxAvarCache)
+import Perspectives.EntiteitAndRDFAliases (ContextID)
+import Perspectives.Identifiers (Namespace, LocalName)
+import Perspectives.PerspectEntiteit (cacheUncachedEntiteit)
+import Perspectives.PerspectivesState (MonadPerspectives)
+import Perspectives.ResourceRetrieval (saveUnversionedEntiteit)
+import Perspectives.Syntax (ContextRecord, PerspectContext(..), PerspectRol(..), RolRecord, binding)
+import Prelude (discard, ($), (<>), bind)
+
+setupUser :: forall e. MonadPerspectives (AjaxAvarCache (now :: NOW | e)) Unit
+setupUser = do
+  cacheUncachedEntiteit "model:User$Systeem"
+    (PerspectContext $ contextRecord "model:User" "Systeem" "model:Systeem$Systeem")
+  cacheUncachedEntiteit "model:User$Systeem_BuitenRol"
+    (PerspectRol $ buitenRolRecord  "model:User" "Systeem")
+  cacheUncachedEntiteit "model:User$Me"
+    (PerspectRol $ rolRecord "model:Systeem$Systeem$gebruiker" "model:User" "Me")
+  cacheUncachedEntiteit "model:User$TrustedCluster"
+    (PerspectContext $ contextRecord "model:User" "TrustedCluster" "model:Systeem$TrustedCluster")
+
+  (_ :: PerspectContext) <- saveUnversionedEntiteit "model:User$Systeem"
+  (_ :: PerspectRol) <- saveUnversionedEntiteit "model:User$Systeem_BuitenRol"
+  (_ :: PerspectRol) <- saveUnversionedEntiteit "model:User$Me"
+  (_ :: PerspectContext) <- saveUnversionedEntiteit "model:User$TrustedCluster"
+
+  runInTransactie do
+    addRol "model:User$Systeem" "model:User$Systeem$gebruiker" "model:User$Me"
+
+contextRecord :: Namespace -> LocalName -> ContextID -> ContextRecord
+contextRecord ns ln tp = defaultContextRecord
+  { _id = ns <> "$" <> ln
+  , displayName = ln
+  , pspType = tp
+  , binnenRol = PerspectRol $ defaultRolRecord
+    { _id = ns <> "$" <> ln <> "_BinnenRol"
+    , pspType = "model:Perspectives$BinnenRol"
+    , binding = binding $ ns <> "$" <> ln <> "_BuitenRol"
+    }
+  , buitenRol = ns <> "$" <> ln <> "_BuitenRol"
+  }
+
+rolRecord :: ContextID -> Namespace -> LocalName -> RolRecord
+rolRecord tp ns ln = defaultRolRecord
+  { _id = ns <> "$" <> ln <> "_BuitenRol"
+  , pspType = "model:Perspectives$BuitenRol"
+  , context = ns <> "$" <> ln
+  }
+
+buitenRolRecord :: Namespace -> LocalName -> RolRecord
+buitenRolRecord = rolRecord "model:Perspectives$BuitenRol"
