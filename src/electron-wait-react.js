1f7dcd6990769c6a5a6a2a8b9dc5c49f2be134bb
Joop Ringelberg <joop.ringelberg@gmail.com>
2018-03-15 21:39:34 +0100

    Testing new code formatting
    Formatting is now in accordance with ESLint settings.

--- src/electron-wait-react.js
+++ src/electron-wait-react.js
@@ -1,27 +1,32 @@
-const net = require('net')
+const net = require("net");
 const port = process.env.PORT ? (
   process.env.PORT - 100
-) : 3000
+) : 3000;
 
-process.env.ELECTRON_START_URL = `http://localhost:${port}`
+process.env.ELECTRON_START_URL = `http://localhost:${port}`;
 
-const client = new net.Socket()
+const client = new net.Socket();
 
-let startedElectron = false
-const tryConnection = () => client.connect({port: port}, () => {
-                                             client.end()
-                                             if (!startedElectron)
-                                             {
-                                               console.log('starting electron')
-                                               startedElectron = true
-                                               const exec = require('child_process').exec
-                                               exec('npm run electron')
-                                             }
-                                           }
-)
+let startedElectron = false;
+const tryConnection = () =>
+  client.connect(
+    {port: port},
+    () =>
+    {
+      client.end();
+      if (!startedElectron)
+      {
+        console.log("starting electron");
+        startedElectron = true;
+        const exec = require("child_process").exec;
+        exec("npm run electron");
+      }
+    }
+  );
 
-tryConnection()
+tryConnection();
 
-client.on('error', (error) => {
-  setTimeout(tryConnection, 1000)
-})
+client.on("error", (error) =>
+{
+  setTimeout(tryConnection, 1000);
+});
