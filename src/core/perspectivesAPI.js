96e1042422f56c053f75b82d3955e3838a791e8f
Joop Ringelberg <joop.ringelberg@gmail.com>
2018-03-16 10:11:31 +0100

    Adding ace resources etc
    Also resolved issue with module reference in perspectivesAPI.js
    Actualized documentation for building Ace.

--- src/core/perspectivesAPI.js
+++ src/core/perspectivesAPI.js
@@ -1,4 +1,1 @@
-// referred to from ./output/Perspectives.Api/foreign.js
-// exports.connect = require("../client/perspectivesApiProxy.js").connect;
-import connect from "../client/perspectivesApiProxy.js";
-export {connect};
+exports.connect = require("client/perspectivesApiProxy.js").connect;
