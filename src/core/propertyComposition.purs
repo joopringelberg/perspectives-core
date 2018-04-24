741a0fb257592b01272dd56c778afbeed6e1d19d
Joop Ringelberg <joop.ringelberg@gmail.com>
2018-04-24 15:50:44 +0200

    Lazy property composition
    To be used for recursive queries.

--- src/core/propertyComposition.purs
+++ src/core/propertyComposition.purs
@@ -1,18 +1,18 @@
 module Perspectives.PropertyComposition where
 
 
-import Data.Array (cons, difference, nub)
+import Data.Array (cons, difference, head, nub)
+import Data.Maybe (Maybe(..))
 import Data.Traversable (traverse)
-import Perspectives.CoreTypes (Triple(..), TripleGetter, TypedTripleGetter(..))
+import Perspectives.CoreTypes (Triple(..), TripleGetter, TypedTripleGetter(..), typedTripleGetterName)
 import Perspectives.TripleAdministration (getRef, memorize)
-import Prelude (bind, join, pure, ($), (<>), map)
+import Prelude (Unit, bind, join, map, pure, unit, ($), (<>), (>>>))
 
 compose :: forall e.
   TypedTripleGetter e ->
   TypedTripleGetter e ->
   TypedTripleGetter e
 compose (TypedTripleGetter nameOfp p) (TypedTripleGetter nameOfq q) =
-  -- TODO: domain2 must subsume range1
   memorize getter name
     where
     getter :: TripleGetter e
@@ -33,3 +33,33 @@ compose (TypedTripleGetter nameOfp p) (TypedTripleGetter nameOfq q) =
     name = "(" <>  nameOfp <> " >-> " <> nameOfq <> ")"
 
 infixl 9 compose as >->
+
+composeLazy :: forall e.
+  TypedTripleGetter e ->
+  (Unit -> TypedTripleGetter e) ->
+  TypedTripleGetter e
+composeLazy (TypedTripleGetter nameOfp p) g =
+  memorize getter name
+    where
+    getter :: TripleGetter e
+    getter id = do
+      t@(Triple{object : objectsOfP}) <- p id
+      case head objectsOfP of
+        Nothing -> pure t
+        otherwise -> do
+          (TypedTripleGetter nameOfq q) <- pure (g unit)
+          -- NOTE: (difference objectsOfP [id]) is our safety catch for cyclic graphs.
+          (triples :: Array (Triple e)) <- traverse q (difference objectsOfP [id])
+          -- some t' in triples may have zero objects under q. Their subjects contribute nothing to the objects of the composition.
+          objects <- pure $ nub $ join $ map (\(Triple{object}) -> object) triples
+          pure $ Triple { subject: id
+                        , predicate : name
+                        , object : objects
+                        , dependencies : []
+                        , supports : map getRef (cons t triples)
+                        , tripleGetter : getter}
+
+    name :: String
+    name = "(" <>  nameOfp <> " >-> " <> (typedTripleGetterName (g unit)) <> ")"
+
+infixl 9 composeLazy as >->>
