4c559ed19c130f24e7a41129440b3e6ac29bb8fc
Joop Ringelberg <joop.ringelberg@gmail.com>
2018-04-24 20:02:28 +0200

    queryCompiler: constructQueryFunction now is MonadPerspectives.

--- src/core/queryCompiler.purs
+++ src/core/queryCompiler.purs
@@ -3,11 +3,10 @@ module Perspectives.QueryCompiler where
 import Control.Monad.Eff.Exception (Error, error)
 import Control.Monad.Trans.Class (lift)
 import Data.Array (foldl, head, unsnoc)
-import Data.Foldable (foldM)
 import Data.Maybe (Maybe, fromJust)
 import Data.Traversable (traverse)
 import Partial.Unsafe (unsafePartial)
-import Perspectives.CoreTypes (MonadPerspectives, MonadPerspectivesQuery, MonadPerspectivesQueryCompiler, ObjectsGetter, TypedTripleGetter, getQueryStepDomain, runMonadPerspectivesQueryCompiler, withQueryCompilerEnvironment)
+import Perspectives.CoreTypes (MonadPerspectives, ObjectsGetter, TypedTripleGetter, MonadPerspectivesQuery)
 import Perspectives.Effects (AjaxAvarCache)
 import Perspectives.EntiteitAndRDFAliases (ContextID, ID, PropertyName, RolName)
 import Perspectives.Identifiers (LocalName, deconstructLocalNameFromDomeinURI)
@@ -19,19 +18,21 @@ import Perspectives.TripleGetter (constructExternalPropertyGetter, constructExte
 import Perspectives.Utilities (onNothing, onNothing')
 import Prelude (bind, pure, ($), (<$>), (<*>), (<<<), (<>), (>=>), (>>=))
 
--- | From a qualified name for a Rol and its context, construct a function that computes the instances of that Rol for a given context.
+-- | From a qualified name for a Rol and the context that it is defined in (hence, the domain of the rol-getter), construct a function that computes the instances of that Rol for a given context.
 -- | The Rol may be defined as computed.
 rolQuery  :: forall e.
   RolName ->
   ContextID ->
   MonadPerspectivesQuery (AjaxAvarCache e) (TypedTripleGetter e)
 rolQuery rn cid = do
+  -- If the type of the role is Property, start with
+
   -- Is the type of rolType rn or one of its ancestors q:Query?
   (isAQuery :: Boolean) <- (toBoolean (contains "model:QueryAst$Query" (closure contextType)) rn)
   if isAQuery
     -- TODO: memorize!
     then do
-      tg <- runMonadPerspectivesQueryCompiler cid (constructQueryFunction rn)
+      tg <- lift $ constructQueryFunction rn
       -- the identity TypedTripleGetter constructs a triple <subject identity subject> that is saved
       -- and can be found with TripleRef <subject> "model:Perspectives$identity". This TripleRef is stored in
       -- the query variable "#context".
@@ -53,10 +54,11 @@ propertyQuery  :: forall e.
   MonadPerspectivesQuery (AjaxAvarCache e) (TypedTripleGetter e)
 propertyQuery pn cid = do
   -- Is the type of propertyType or one of its ancestors q:Query?
+  -- TODO. Dit is niet langer een goede test of iets een query is!
   (isAQuery :: Boolean) <- (toBoolean (contains "model:QueryAst$Query" (closure contextType)) pn)
   if isAQuery
     then do
-      tg <- runMonadPerspectivesQueryCompiler pn (constructQueryFunction pn)
+      tg <- lift $ constructQueryFunction pn
       pure $ (saveVar "#rol" (identity >-> tg))
     else do
       domain <- lift $ getContextTypeF pn
@@ -71,11 +73,10 @@ propertyQuery pn cid = do
 -- TODO: voeg state toe waarin bijgehouden wordt welke variabelen al gedefinieerd zijn, zodat je kunt stoppen als vooruit verwezen wordt. Houdt daar ook het domein van de querystap bij.
 constructQueryFunction :: forall e.
   ContextID ->
-  MonadPerspectivesQueryCompiler (AjaxAvarCache e) (TypedTripleGetter e)
+  MonadPerspectives (AjaxAvarCache e) (TypedTripleGetter e)
 constructQueryFunction typeDescriptionID = do
-  queryStepType <- lift $ lift $ onNothing (errorMessage "no type found" "")
+  queryStepType <- onNothing (errorMessage "no type found" "")
     (firstOnly getContextType typeDescriptionID)
-  domain <- getQueryStepDomain
   case queryStepType of
     "model:QueryAst$constructExternalPropertyGetter" ->
       applyPropertyConstructor constructExternalPropertyGetter queryStepType
@@ -92,39 +93,39 @@ constructQueryFunction typeDescriptionID = do
     "model:QueryAst$constructRolLookup" ->
       applyPropertyConstructor constructRolLookup queryStepType
     "model:QueryAst$constructRolGetter" -> do
-      rolName <- lift $ lift $ onNothing (errorMessage "no rolName" queryStepType) (firstOnly (getRol "model:QueryAst$constructRolGetter$rol") typeDescriptionID)
+      rolName <- onNothing (errorMessage "no rolName" queryStepType) (firstOnly (getRol "model:QueryAst$constructRolGetter$rol") typeDescriptionID)
       pure $ constructRolGetter rolName
     "model:QueryAst$constructInverseRolGetter" -> do
-      rolName <- lift $ lift $ onNothing (errorMessage "no rolName" queryStepType) (firstOnly (getRol "model:QueryAst$constructRolGetter$rol") typeDescriptionID)
+      rolName <- onNothing (errorMessage "no rolName" queryStepType) (firstOnly (getRol "model:QueryAst$constructRolGetter$rol") typeDescriptionID)
       pure $ constructInverseRolGetter rolName
     "model:QueryAst$rolesOf" ->
-      lift $ lift $ rolesOf <$> (onNothing (errorMessage "no context" queryStepType) (firstOnly (getRol "model:QueryAst$rolesOf$context") queryStepType))
+      rolesOf <$> (onNothing (errorMessage "no context" queryStepType) (firstOnly (getRol "model:QueryAst$rolesOf$context") queryStepType))
     "model:QueryAst$notEmpty" -> applyUnaryCombinator notEmpty queryStepType
     "model:QueryAst$closure" -> applyUnaryCombinator closure queryStepType
     "model:QueryAst$closure'" -> applyUnaryCombinator closure' queryStepType
     "model:QueryAst$lastElement'" -> applyUnaryCombinator lastElement queryStepType
     "model:QueryAst$compose" -> applyBinaryCombinator compose queryStepType
     "model:QueryAst$concat" -> do
-      (operandIds :: Array ID) <- lift $ lift $ getRolByLocalName "operand" typeDescriptionID
+      (operandIds :: Array ID) <- getRolByLocalName "operand" typeDescriptionID
       (operands :: Array (TypedTripleGetter e)) <- traverse constructQueryFunction operandIds
       {init, last} <- onNothing' (errorMessage "too few operands" queryStepType) (unsnoc operands)
-      lift $ lift $ foldM concat last init
+      pure $ foldl concat last init
     "model:QueryAst$filter" -> do
-      criteriumId <- lift $ lift $ onNothing (errorMessage "no criterium" queryStepType)
+      criteriumId <- onNothing (errorMessage "no criterium" queryStepType)
         (firstOnly (getRolByLocalName "criterium") typeDescriptionID)
-      candidateId <- lift $ lift $ onNothing (errorMessage "no candidates" queryStepType)
+      candidateId <- onNothing (errorMessage "no candidates" queryStepType)
         (firstOnly (getRolByLocalName "criterium") typeDescriptionID)
-      filter <$> (constructQueryFunction criteriumId) <*> withQueryCompilerEnvironment (constructQueryFunction candidateId)
+      filter <$> (constructQueryFunction criteriumId) <*> constructQueryFunction candidateId
     "model:QueryAst$Constant" -> do
-      lift $ lift $ constant <$> onNothing (errorMessage "no constant value provided" queryStepType) (firstOnly (getExternalProperty "model:QueryAst$Constant$value") typeDescriptionID)
+      constant <$> onNothing (errorMessage "no constant value provided" queryStepType) (firstOnly (getExternalProperty "model:QueryAst$Constant$value") typeDescriptionID)
     "model:QueryAst$Variable" -> do
-      variableName <- lift $ lift $ onNothing (errorMessage "no variable name found" queryStepType)
+      variableName <- onNothing (errorMessage "no variable name found" queryStepType)
         (firstOnly (getInternalProperty "model:QueryAst$Variable$name") typeDescriptionID)
       pure $ ref variableName
     "model:QueryAst$setVariable" -> do
-      variableName <- lift $ lift $ onNothing (errorMessage "no variable name found" queryStepType)
+      variableName <- onNothing (errorMessage "no variable name found" queryStepType)
         (firstOnly (getInternalProperty "model:QueryAst$Variable$name") typeDescriptionID)
-      valueDescriptionID <- lift $ lift $ onNothing (errorMessage "no value found" queryStepType)
+      valueDescriptionID <- onNothing (errorMessage "no value found" queryStepType)
         (firstOnly (getRolByLocalName "value") typeDescriptionID)
       valueQuery <- constructQueryFunction valueDescriptionID
       pure $ var variableName valueQuery
@@ -135,25 +136,25 @@ constructQueryFunction typeDescriptionID = do
   where
     applyPropertyConstructor :: (PropertyName -> TypedTripleGetter e)
       -> ID
-      -> MonadPerspectivesQueryCompiler (AjaxAvarCache e) (TypedTripleGetter e)
+      -> MonadPerspectives (AjaxAvarCache e) (TypedTripleGetter e)
     applyPropertyConstructor f queryStepType = do
-      propertyId <- lift $ lift $ onNothing (errorMessage "no property found" queryStepType)
+      propertyId <- onNothing (errorMessage "no property found" queryStepType)
         (firstOnly (getRolByLocalName "property") typeDescriptionID)
       pure $ f propertyId
 
     applyUnaryCombinator :: (TypedTripleGetter e -> TypedTripleGetter e )
       -> ID
-      -> MonadPerspectivesQueryCompiler (AjaxAvarCache e) (TypedTripleGetter e)
+      -> MonadPerspectives (AjaxAvarCache e) (TypedTripleGetter e)
     applyUnaryCombinator c queryStepType = do
-      queryId <- lift $ lift $ onNothing (errorMessage "no query found" queryStepType)
+      queryId <- onNothing (errorMessage "no query found" queryStepType)
         (firstOnly (getRolByLocalName "query") typeDescriptionID)
       constructQueryFunction queryId >>= pure <<< c
 
     applyBinaryCombinator :: (TypedTripleGetter e -> TypedTripleGetter e -> TypedTripleGetter e)
       -> ID
-      -> MonadPerspectivesQueryCompiler (AjaxAvarCache e) (TypedTripleGetter e)
+      -> MonadPerspectives (AjaxAvarCache e) (TypedTripleGetter e)
     applyBinaryCombinator c queryStepType = do
-      (operandIds :: Array ID) <- lift $ lift $ getRolByLocalName "operand" typeDescriptionID
+      (operandIds :: Array ID) <- getRolByLocalName "operand" typeDescriptionID
       operands <- traverse constructQueryFunction operandIds
       {init, last} <- onNothing' (errorMessage "too few operands" queryStepType) (unsnoc operands)
       pure $ foldl c last init
