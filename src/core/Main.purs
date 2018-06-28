ccfb6880272182144490db86fe2cb1c2cbc4cbf9
Joop Ringelberg <joop.ringelberg@gmail.com>
2018-06-28 20:01:14 +0200

    Removed query string analysis
    We need a different way to authenticate on a headless core.

--- src/core/Main.purs
+++ src/core/Main.purs
@@ -21,51 +21,27 @@
 
 module Main where
 import Control.Aff.Sockets (SOCKETIO)
-import Control.Monad.Aff (Aff, Error, error, forkAff, liftEff', runAff, throwError)
+import Control.Monad.Aff (Error, forkAff, runAff)
 import Control.Monad.Aff.AVar (AVar, makeVar)
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (CONSOLE, log)
 import DOM (DOM)
-import DOM.HTML (window)
-import DOM.HTML.Location (search)
-import DOM.HTML.Window (location)
 import Data.Either (Either(..))
-import Data.Maybe (Maybe(..))
-import Data.StrMap (fromFoldable, lookup)
-import Data.Tuple (Tuple(..))
-import URI.Query (Query(..), parser) as URI
 import Perspectives.Api (setupApi, setupTcpApi)
-import Perspectives.Couchdb (User, Password)
-import Perspectives.Effects (AjaxAvarCache, AvarCache, REACT)
+import Perspectives.Effects (AjaxAvarCache, REACT)
 import Perspectives.PerspectivesState (newPerspectivesState, runPerspectivesWithState)
-import Prelude (Unit, bind, pure, ($), (<$>), (>>=), (<>), show, void, discard)
-import Text.Parsing.Parser.String (ParseError, runParser)
+import Prelude (Unit, bind, pure, ($), (<>), show, void, discard)
 
 main :: Eff (AjaxAvarCache (console :: CONSOLE, dom :: DOM, react :: REACT, socketio :: SOCKETIO)) Unit
 main = void $ runAff handleError do
-  mt <- credentialsFromQueryString
-  case mt of
-    Nothing -> throwError $ error "Both the user and password key-value pairs have to be present in the query string!"
-    (Just (Tuple usr pwd)) -> do
-      -- TODO: retrieve the couchdb credentials from the trusted cluster or through the user interface.
-      url <- pure "http://127.0.0.1:5984/"
-      (av :: AVar String) <- makeVar "This value will be removed on first authentication!"
-      state <- makeVar $ newPerspectivesState {userName: usr, couchdbPassword: pwd, couchdbBaseURL: url} av
-      void $ forkAff $ runPerspectivesWithState setupApi state
-      void $ forkAff $ runPerspectivesWithState setupTcpApi state
-
--- TODO. Als geen waarde in usr beschikbaar is, moet de gebruiker een naam (opnieuw) invoeren!
-credentialsFromQueryString :: forall e. Aff (AvarCache (dom :: DOM | e)) (Maybe (Tuple User Password))
-credentialsFromQueryString = do
-  (parseResult :: Either ParseError URI.Query) <- liftEff' ((runParser URI.parser) <$> (window >>= location >>= search))
-  case parseResult of
-    (Left m) -> pure Nothing
-    (Right (URI.Query kvp)) -> case lookup "user" $ fromFoldable kvp of
-      (Just (Just usr)) ->
-        case lookup "password" $ fromFoldable kvp of
-          (Just (Just pwd)) -> pure $ Just (Tuple usr pwd)
-          otherwise -> pure Nothing
-      otherwise -> pure Nothing
+  -- TODO: retrieve the couchdb credentials from the trusted cluster or through the user interface.
+  usr <- pure "cor"
+  pwd <- pure "geheim"
+  url <- pure "http://127.0.0.1:5984/"
+  (av :: AVar String) <- makeVar "This value will be removed on first authentication!"
+  state <- makeVar $ newPerspectivesState {userName: usr, couchdbPassword: pwd, couchdbBaseURL: url} av
+  void $ forkAff $ runPerspectivesWithState setupApi state
+  void $ forkAff $ runPerspectivesWithState setupTcpApi state
 
 handleError :: forall e a. (Either Error a -> Eff (console :: CONSOLE | e) Unit)
 handleError (Left e) = log $ "An error condition: " <> (show e)
