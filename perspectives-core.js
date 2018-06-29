bf0a7e13982b87131f865da5d23648d54c8f4806
Joop Ringelberg <joop.ringelberg@gmail.com>
2018-06-29 12:38:01 +0200

    packaging
    Added .npmignore to exclude almost everything except the bundle from 
    being installed by npm.

--- perspectives-core.js
+++ perspectives-core.js
@@ -266,98 +266,270 @@ exports.clearImmediate = typeof clearImmediate === "function" ? clearImmediate :
 };
 }).call(this,require("timers").setImmediate,require("timers").clearImmediate)
 },{"process/browser.js":2,"timers":3}],4:[function(require,module,exports){
-/*
-object-assign
-(c) Sindre Sorhus
-@license MIT
-*/
+let resolver, rejecter;
 
-'use strict';
-/* eslint-disable no-unused-vars */
-var getOwnPropertySymbols = Object.getOwnPropertySymbols;
-var hasOwnProperty = Object.prototype.hasOwnProperty;
-var propIsEnumerable = Object.prototype.propertyIsEnumerable;
+const Perspectives = new Promise(
+  function (resolve, reject)
+  {
+    resolver = resolve;
+    rejecter = reject;
+  });
 
-function toObject(val) {
-	if (val === null || val === undefined) {
-		throw new TypeError('Object.assign cannot be called with null or undefined');
-	}
-
-	return Object(val);
+// This function will be called from Perspectives Core if it want to set up an internal channel to a GUI.
+function createRequestEmitterImpl (left, right, emit)
+{
+  try
+  {
+    // Resolve the Perspectives promise made above for the proxy.
+    resolver(new PerspectivesProxy(new InternalChannel(left, right, emit)));
+  }
+  catch(e)
+  {
+    rejecter(e);
+  }
 }
 
-function shouldUseNative() {
-	try {
-		if (!Object.assign) {
-			return false;
-		}
-
-		// Detect buggy property enumeration order in older V8 versions.
-
-		// https://bugs.chromium.org/p/v8/issues/detail?id=4118
-		var test1 = new String('abc');  // eslint-disable-line no-new-wrappers
-		test1[5] = 'de';
-		if (Object.getOwnPropertyNames(test1)[0] === '5') {
-			return false;
-		}
-
-		// https://bugs.chromium.org/p/v8/issues/detail?id=3056
-		var test2 = {};
-		for (var i = 0; i < 10; i++) {
-			test2['_' + String.fromCharCode(i)] = i;
-		}
-		var order2 = Object.getOwnPropertyNames(test2).map(function (n) {
-			return test2[n];
-		});
-		if (order2.join('') !== '0123456789') {
-			return false;
-		}
-
-		// https://bugs.chromium.org/p/v8/issues/detail?id=3056
-		var test3 = {};
-		'abcdefghijklmnopqrst'.split('').forEach(function (letter) {
-			test3[letter] = letter;
-		});
-		if (Object.keys(Object.assign({}, test3)).join('') !==
-				'abcdefghijklmnopqrst') {
-			return false;
-		}
-
-		return true;
-	} catch (err) {
-		// We don't expect any of the above to throw, but better to be safe.
-		return false;
-	}
+// Top level entry function to set up a TCP channel with a Perspectives Core endpoint.
+// From module Control.Aff.Sockets:
+// type TCPOptions opts = {port :: Port, host :: Host, allowHalfOpen :: Boolean | opts}
+// type Port = Int
+// type Host = String
+function createTcpConnectionToPerspectives (options)
+{
+  // Resolve the Perspectives promise made above for the proxy.
+  resolver(new PerspectivesProxy(new TcpChannel(options)));
+  try
+  {
+    // Resolve the Perspectives promise made above for the proxy.
+    resolver(new PerspectivesProxy(new TcpChannel(options)));
+  }
+  catch (e)
+  {
+    rejecter(e);
+  }
 }
 
-module.exports = shouldUseNative() ? Object.assign : function (target, source) {
-	var from;
-	var to = toObject(target);
-	var symbols;
+class TcpChannel
+{
+  constructor (options)
+  {
+    let connection;
+    const valueReceivers = {};
+    this.connection = require("net").createConnection(
+      options,
+      // message will be in base64. Appending a string to it converts it to a new string.
+      function (message)
+      {
+        const {setterId, objects} = JSON.parse(message + "");
+        valueReceivers[setterId](objects);
+      });
+    connection = this.connection;
+    this.valueReceivers = valueReceivers;
+    this.connection.on(
+      "error",
+      function (error)
+      {
+        connection.close(
+          function ()
+          {
+            // We cannot signal the server, because the connection is in error.
+            window.log( "Connection to Perspectives server is closed because: " + error);
+          });
+      });
+    this.connection.on(
+      "end",
+      function ()
+      {
+        // Emitted when the other end of the socket sends a FIN packet.
+        // By default (allowHalfOpen == false) the socket will destroy its file
+        // descriptor once it has written out its pending write queue.
+        // Hence, we need not signal the server that this message emitter shuts down.
+      });
 
-	for (var s = 1; s < arguments.length; s++) {
-		from = Object(arguments[s]);
+  }
 
-		for (var key in from) {
-			if (hasOwnProperty.call(from, key)) {
-				to[key] = from[key];
-			}
-		}
+  nextRequestId ()
+  {
+    this.requestId = this.requestId + 1;
+    return this.requestId;
+  }
 
-		if (getOwnPropertySymbols) {
-			symbols = getOwnPropertySymbols(from);
-			for (var i = 0; i < symbols.length; i++) {
-				if (propIsEnumerable.call(from, symbols[i])) {
-					to[symbols[i]] = from[symbols[i]];
-				}
-			}
-		}
-	}
+  // close will lead messageProducer to receive (Right unit).
+  close()
+  {
+    this.connection.write("shutdown");
+    this.connection.end();
+    this.send = function()
+    {
+      throw( "This client has shut down!");
+    };
+  }
 
-	return to;
+  // req has the following format (taken from: module Perspectives.Api)
+  //   { request :: String
+  //   , subject :: String
+  //   , predicate :: String
+  //   , setterId :: ReactStateSetterIdentifier}
+  // type ReactStateSetterIdentifier = String
+  send(req, receiveValues)
+  {
+    req.setterId = this.nextRequestId();
+    this.valueReceivers[ req.setterId ] = receiveValues;
+    this.connection.write(JSON.stringify(req));
+  }
+
+  unsubscribe(req)
+  {
+    delete this.valueReceivers[req.setterId];
+    this.connection.write(
+      {request: "Unsubscribe", subject: req.subject, predicate: req.predicate, setterId: req.setterId}
+    );
+  }
+}
+
+class InternalChannel
+{
+  constructor (left, right, emit)
+  {
+    this.left = left;
+    this.right = right;
+    this.emit = emit;
+    this.requestId = -1;
+  }
+
+  nextRequestId ()
+  {
+    this.requestId = this.requestId + 1;
+    return this.requestId;
+  }
+
+  // Inform the server that this client shuts down.
+  // No other requests may follow this message.
+  close()
+  {
+    this.emit( this.right({}) )();
+    this.emit = function()
+    {
+      throw( "This client has shut down!");
+    };
+  }
+
+  send (req, receiveValues)
+  {
+    const proxy = this;
+    // Create a correlation identifier and store 'receiveValues' with it.
+    // Send the correlation identifier instead of reactStateSetter.
+    req.reactStateSetter = function (arrString)
+    {
+      receiveValues(arrString);
+      return function () {};
+    };
+    req.setterId = this.nextRequestId();
+    this.emit( this.left(req) )();
+    // return the unsubscriber.
+    return function()
+    {
+      proxy.unsubscribe( req );
+    };
+  }
+
+  unsubscribe(req)
+  {
+    this.send(
+      {request: "Unsubscribe", subject: req.subject, predicate: req.predicate, setterId: req.setterId}
+    );
+  }
+
+}
+
+class PerspectivesProxy
+{
+  constructor (channel)
+  {
+    this.channel = channel;
+  }
+
+  // Inform the server that this client shuts down.
+  // No other requests may follow this message.
+  close()
+  {
+    this.channel.close();
+  }
+
+  send (req, receiveValues)
+  {
+    this.channel.send( req, receiveValues );
+  }
+
+  unsubscribe (req)
+  {
+    this.channel.unsubscribe(req);
+  }
+
+  getRolBinding (contextID, rolName, receiveValues)
+  {
+    this.send(
+      {request: "GetRolBinding", subject: contextID, predicate: rolName},
+      receiveValues);
+  }
+
+  getRol (contextID, rolName, receiveValues)
+  {
+    this.send(
+      {request: "GetRol", subject: contextID, predicate: rolName},
+      receiveValues);
+  }
+
+  getProperty (rolID, propertyName, receiveValues)
+  {
+    this.send(
+      {request: "GetProperty", subject: rolID, predicate: propertyName},
+      receiveValues);
+  }
+
+  getBinding (rolID, receiveValues)
+  {
+    this.send(
+      {request: "GetBinding", subject: rolID, predicate: ""},
+      receiveValues);
+  }
+
+  getBindingType (rolID, receiveValues)
+  {
+    this.send(
+      {request: "GetBindingType", subject: rolID, predicate: ""},
+      receiveValues);
+  }
+
+  getViewProperties (viewName, receiveValues)
+  {
+    this.send(
+      {request: "GetViewProperties", subject: viewName, predicate: ""},
+      receiveValues);
+  }
+
+  getRolContext (rolID, receiveValues)
+  {
+    this.send(
+      {request: "GetRolContext", subject: rolID, predicate: ""},
+      receiveValues);
+  }
+
+  getContextType (contextID, receiveValues)
+  {
+    this.send(
+      {request: "GetContextType", subject: contextID, predicate: ""},
+      receiveValues);
+  }
+}
+
+module.exports = {
+  Perspectives: Perspectives,
+  createRequestEmitterImpl: createRequestEmitterImpl,
+  createTcpConnectionToPerspectives: createTcpConnectionToPerspectives
 };
 
-},{}],5:[function(require,module,exports){
+},{"net":1}],5:[function(require,module,exports){
 function createConnectionEmitterImpl(left, right, options, emit)
 {
   const server = require('net').createServer(options);
@@ -37090,11 +37262,10 @@ if (exports === undefined)
 {
   exports = module.exports;
 }
-const x = require( "object-assign");
-// exports.createRequestEmitterImpl = require("perspectives-client").createRequestEmitterImpl;
-exports.createRequestEmitterImpl = function(){};
 
-},{"object-assign":4}],298:[function(require,module,exports){
+exports.createRequestEmitterImpl = require("perspectives-proxy").createRequestEmitterImpl;
+
+},{"perspectives-proxy":4}],298:[function(require,module,exports){
 // Generated by purs version 0.11.7
 "use strict";
 var $foreign = require("./foreign");
--- src/core/perspectivesAPI.js
+++ src/core/perspectivesAPI.js
@@ -2,6 +2,5 @@ if (exports === undefined)
 {
   exports = module.exports;
 }
-const x = require( "object-assign");
-// exports.createRequestEmitterImpl = require("perspectives-client").createRequestEmitterImpl;
-exports.createRequestEmitterImpl = function(){};
+
+exports.createRequestEmitterImpl = require("perspectives-proxy").createRequestEmitterImpl;
