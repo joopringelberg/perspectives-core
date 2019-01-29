8daa567f22e976973665a13ce99818043a85a7ca
Joop Ringelberg <joop.ringelberg@gmail.com>
2019-01-29 12:53:39 +0100

    Module dependencies
    Computing an ordered list of modules according to import relations.

--- test/Main.purs
+++ test/Main.purs
@@ -4,14 +4,15 @@ import Control.Monad.Aff (Fiber, runAff)
 import Control.Monad.Eff (Eff)
 import Perspectives.PerspectivesState (runPerspectives)
 import Prelude (Unit)
-import Test.ParseFiles (test)
+import Test.SortModules (test)
 import Test.TestEffects as TE
 import Node.FS (FS)
 
 -- import Test.BoundContexts
 
-main :: forall e. Eff (TE.TestEffects (fs :: FS | e)) (Fiber (TE.TestEffects (fs :: FS | e)) Unit)
-main = runAff TE.handleError (runPerspectives "cor" "geheim" test)
+main = test
+-- main :: forall e. Eff (TE.TestEffects (fs :: FS | e)) (Fiber (TE.TestEffects (fs :: FS | e)) Unit)
+-- main = runAff TE.handleError (runPerspectives "cor" "geheim" test)
 
 -- runTest :: forall e a. Show a => Aff (console :: AC.CONSOLE | e) a -> Eff (console :: AC.CONSOLE | e) Unit
 -- runTest t =
