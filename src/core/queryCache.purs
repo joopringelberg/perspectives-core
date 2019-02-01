72462469b53af3a6f35043a6020790da9b030398
Joop Ringelberg <joop.ringelberg@gmail.com>
2019-02-01 19:50:37 +0100

    QueryCache

--- src/core/queryCache.purs
+++ src/core/queryCache.purs
@@ -7,18 +7,18 @@ import Perspectives.CoreTypes (MonadPerspectives, QueryCache, TypedTripleGetter)
 import Perspectives.GlobalUnsafeStrMap (GLOBALMAP, new, peek, poke)
 import Prelude (pure, unit, ($), bind)
 
-queryCache :: forall s p o c r b e. (QueryCache s p o c r b e)
+queryCache :: forall s p o e. (QueryCache s p o e)
 queryCache = new unit
 
-queryCacheInsert :: forall s p o c r b e1 e2.
+queryCacheInsert :: forall s p o e1 e2.
   String ->
-  TypedTripleGetter s p o c r b e1 ->
-  MonadPerspectives c r b (gm :: GLOBALMAP | e2) (TypedTripleGetter s p o c r b e1)
+  TypedTripleGetter s p o e1 ->
+  MonadPerspectives (gm :: GLOBALMAP | e2) (TypedTripleGetter s p o e1)
 queryCacheInsert qname getter = do
   _ <- lift $ liftEff $ poke queryCache qname getter
   pure getter
 
-queryCacheLookup :: forall s p o c r b e1 e2. 
+queryCacheLookup :: forall s p o e1 e2.
   String ->
-  MonadPerspectives c r b (gm :: GLOBALMAP | e1) (Maybe (TypedTripleGetter s p o c r b e2))
+  MonadPerspectives (gm :: GLOBALMAP | e1) (Maybe (TypedTripleGetter s p o e2))
 queryCacheLookup qname = liftEff $ peek queryCache qname
