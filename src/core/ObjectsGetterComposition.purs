097100c78be42fcc737e20c3a8999544fedb8be0
Joop Ringelberg <joop.ringelberg@gmail.com>
2019-02-01 22:38:19 +0100

    Composition operators

--- src/core/ObjectsGetterComposition.purs
+++ src/core/ObjectsGetterComposition.purs
@@ -6,11 +6,11 @@ import Data.Newtype (alaF)
 import Perspectives.CoreTypes (ObjectsGetter)
 import Prelude (class Eq, class Show, pure, (<<<), (==), (>=>), (>>=))
 
-unionOfObjects :: forall s o t c r b e.
+unionOfObjects :: forall s o t e.
   Eq o =>
-  ObjectsGetter s t c r b e ->
-  ObjectsGetter t o c r b e ->
-  ObjectsGetter s o c r b e
+  ObjectsGetter s t e ->
+  ObjectsGetter t o e ->
+  ObjectsGetter s o e
 unionOfObjects p q = p >=>
   (\objectsOfP -> foldM
       (\r objectOfP -> q objectOfP >>= pure <<< union r)
@@ -19,11 +19,11 @@ unionOfObjects p q = p >=>
 
 infixl 9 unionOfObjects as /-/
 
-intersectionOfObjects :: forall s o t c r b e.
+intersectionOfObjects :: forall s o t e.
   Eq o =>
-  ObjectsGetter s t c r b e ->
-  ObjectsGetter t o c r b e ->
-  ObjectsGetter s o c r b e
+  ObjectsGetter s t e ->
+  ObjectsGetter t o e ->
+  ObjectsGetter s o e
 intersectionOfObjects p q = p >=>
   (\objectsOfP -> foldM
       (\r objectOfP -> q objectOfP >>= pure <<< intersect r)
@@ -35,15 +35,15 @@ infixl 9 intersectionOfObjects as \-\
 -- | Compose an ObjectsGetter from an ObjectsGetter and a function
 -- | that maps an Array String value to a value that can be shown.
 -- | This function typically folds over a monoid.
-composeMonoidal :: forall s o c r b e a. Show a =>
-  ObjectsGetter s o c r b e
+composeMonoidal :: forall s o e a. Show a =>
+  ObjectsGetter s o e
   -> (Array o -> a)
-  -> ObjectsGetter s a c r b e
+  -> ObjectsGetter s a e
 composeMonoidal p f = p >=> pure <<< singleton <<< f
 
-contains :: forall s o c r b e.
+contains :: forall s o e.
   Eq o =>
   o ->
-  ObjectsGetter s o c r b e ->
-  ObjectsGetter s Boolean c r b e
+  ObjectsGetter s o e ->
+  ObjectsGetter s Boolean e
 contains obj p = p `composeMonoidal` (alaF Disj foldMap ((==) obj))
--- src/core/tripleGetterComposition.purs
+++ src/core/tripleGetterComposition.purs
@@ -11,23 +11,23 @@ import Prelude (class Eq, Unit, bind, join, map, pure, unit, ($), (<>))
 
 -- | Compose two queries like composing two functions.
 -- | `psp:Function -> psp:Function -> psp:Function`
-composeTripleGetters :: forall s p o t c r b e.
+composeTripleGetters :: forall s p o t e.
   Eq t =>
   Eq o =>
   Newtype t String =>
   Newtype s String =>
   Newtype p String =>
-  TypedTripleGetter s p t c r b e ->
-  TypedTripleGetter t p o c r b e ->
-  TypedTripleGetter s p o c r b e
+  TypedTripleGetter s p t e ->
+  TypedTripleGetter t p o e ->
+  TypedTripleGetter s p o e
 composeTripleGetters (TypedTripleGetter nameOfp p) (TypedTripleGetter nameOfq q) =
   memorize getter name
     where
-    getter :: TripleGetter s p o c r b e
+    getter :: TripleGetter s p o e
     getter id = do
       t@(Triple{object : objectsOfP}) <- p id
       -- NOTE: (difference objectsOfP [id]) is our safety catch for cyclic graphs.
-      (triples :: Array (Triple t p o c r b e)) <- traverse q (difference objectsOfP [wrap $ unwrap id])
+      (triples :: Array (Triple t p o e)) <- traverse q (difference objectsOfP [wrap $ unwrap id])
       -- some t' in triples may have zero objects under q. Their subjects contribute nothing to the objects of the composition.
       (objects :: Array o) <- pure $ nub $ join $ map (\(Triple{object}) -> object) triples
       pure $ Triple { subject: id
@@ -46,20 +46,20 @@ infixl 9 composeTripleGetters as >->
 -- | (wrapped in a function). Useful for recursive queries that bottom out
 -- | when the first operator yields no results.
 -- | `psp:Function -> (Unit -> psp:Function) -> String -> psp:Function`
-composeLazy :: forall s p o t c r b e.
+composeLazy :: forall s p o t e.
   Eq t =>
   Eq o =>
   Newtype t String =>
   Newtype s String =>
   Newtype p String =>
-  TypedTripleGetter s p t c r b e ->
-  (Unit -> TypedTripleGetter t p o c r b e) ->
+  TypedTripleGetter s p t e ->
+  (Unit -> TypedTripleGetter t p o e) ->
   String ->
-  TypedTripleGetter s p o c r b e
+  TypedTripleGetter s p o e
 composeLazy (TypedTripleGetter nameOfp p) g nameOfg =
   memorize getter name
     where
-    getter :: TripleGetter s p o c r b e
+    getter :: TripleGetter s p o e
     getter id = do
       t@(Triple tr@{object : objectsOfP}) <- p id
       case head objectsOfP of
@@ -72,7 +72,7 @@ composeLazy (TypedTripleGetter nameOfp p) g nameOfg =
         otherwise -> do
           (TypedTripleGetter nameOfq q) <- pure (g unit)
           -- NOTE: (difference objectsOfP [id]) is our safety catch for cyclic graphs.
-          (triples :: Array (Triple t p o c r b e)) <- traverse q (difference objectsOfP [wrap $ unwrap id])
+          (triples :: Array (Triple t p o e)) <- traverse q (difference objectsOfP [wrap $ unwrap id])
           -- some t' in triples may have zero objects under q. Their subjects contribute nothing to the objects of the composition.
           objects <- pure $ nub $ join $ map (\(Triple{object}) -> object) triples
           pure $ Triple { subject: id
