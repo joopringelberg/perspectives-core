96db96bc473a07491f977776c3a54d3729f38b3c
Joop Ringelberg <joop.ringelberg@gmail.com>
2019-03-12 11:33:20 +0100

    DataTypeObjectGetters tested with on the fly content.

--- test/testDataTypeObjectGetters.purs
+++ test/testDataTypeObjectGetters.purs
@@ -6,7 +6,8 @@ import Control.Monad.Free (Free)
 import Data.Maybe (Maybe(..))
 import Partial.Unsafe (unsafePartial)
 import Perspectives.DataTypeObjectGetters (buitenRol, buitenRol', context, contextType, iedereRolInContext, internePropertyTypen, label, propertyTypen, rolBindingDef, rolType, toSingle, typeVanIedereRolInContext)
-import Perspectives.PerspectivesTypes (BuitenRol(..), ContextRol(..), RolDef(..), RolInContext(..), binding)
+import Perspectives.ObjectsGetterComposition ((/-/))
+import Perspectives.PerspectivesTypes (BuitenRol(..), RolDef(..), RolInContext(..), binding)
 import Test.Perspectives.Utils (TestEffects, addTestContext, assertEqual, p, removeTestContext, u)
 import Test.Unit (TestF, suite, suiteSkip, test, testSkip)
 
@@ -20,37 +21,32 @@ t1 = """{ "id": "u:myContext"
   }"""
 
 theSuite :: forall e. Free (TestF (TestEffects e)) Unit
-theSuite = suite "DataTypeObjectGetters" do
+theSuite = suiteSkip "DataTypeObjectGetters" do
   test "Setting up" (addTestContext t1)
   test "contextType" do
-    assertEqual "The View 'Systeem$gebruiker$volledigeNaam' should have contextDef psp:View as contextType."
-      (contextType (p "Systeem$gebruiker$VolledigeNaam"))
-      [p "View"]
+    assertEqual "Context 'u:myContext' should have contextDef psp:Context as contextType."
+      (contextType (u "myContext"))
+      [p "Context"]
   test "toSingle" do
     assertEqual "toSingle unsafely takes a single value out of getter results"
-      (unsafePartial $ toSingle contextType (p "SimpleValue"))
+      (unsafePartial $ toSingle contextType (u "myContext"))
       (p "Context")
   test "buitenRol" do
-    assertEqual "psp:SimpleValue should have buitenRol psp:SimpleValue_buitenRol"
-      (buitenRol (p "SimpleValue"))
-      [BuitenRol $ p "SimpleValue_buitenRol"]
+    assertEqual "u:myContext should have buitenRol u:myContext_buitenRol"
+      (buitenRol (u "myContext"))
+      [BuitenRol $ u "myContext_buitenRol"]
   test "buitenRol'" do
-    assertEqual "psp:SimpleValue should have buitenRol psp:SimpleValue_buitenRol"
-      (buitenRol' (p "SimpleValue"))
-      (Just (BuitenRol $ p "SimpleValue_buitenRol"))
     assertEqual "No buitenrol for a non-existing context"
       (buitenRol' (p "DoesntExist"))
       (Nothing :: Maybe BuitenRol)
   test "iedereRolInContext" do
-    assertEqual "psp:ContextPrototype has two roles in context"
-      (iedereRolInContext (p "ContextPrototype"))
-      [(p "ContextPrototype$buitenRolBeschrijving_1"),
-      (p "ContextPrototype$binnenRolBeschrijving_1")]
+    assertEqual "u:myContext has a single role in context"
+      (iedereRolInContext (u "myContext"))
+      [(u "myContext$rolInContext_0")]
   test "typeVanIedereRolInContext" do
-    assertEqual "psp:ContextPrototype has two types of role in context"
-      (typeVanIedereRolInContext (p "ContextPrototype"))
-      [(p "Context$buitenRolBeschrijving"),
-      (p "Context$binnenRolBeschrijving")]
+    assertEqual "u:myContext has a single type of role in context"
+      (typeVanIedereRolInContext (u "myContext"))
+      [(p "Context$rolInContext")]
   test "propertyTypen" do
     assertEqual "The BuitenRol of the Rol $mogelijkeBinding of psp:Rol has two properties"
       (propertyTypen (p "Rol$mogelijkeBinding_buitenRol"))
@@ -62,44 +58,32 @@ theSuite = suite "DataTypeObjectGetters" do
         [(p ""),
         (p "")]
   test "label" do
-    assertEqual "psp:SimpleValue has label 'SimpleValue'"
-      (label (p "SimpleValue"))
-      ["SimpleValue"]
     assertEqual "u:myContext has label 'myContext'"
       (label (u "myContext"))
       ["myContext"]
   test "rolType" do
-    assertEqual "Context$rolInContext_1 should have type 'psp:Context$RolInContext'"
-      (rolType (RolInContext $ p "Context$rolInContext_1"))
+    assertEqual "The role in myContext should have type 'psp:Context$RolInContext'"
+      (((iedereRolInContext >=> pure <<< map RolInContext) /-/ rolType) (u "myContext"))
       [RolDef (p "Context$rolInContext")]
+
   -- test "binding" do
   --   assertEqual "Context$rolInContext_1 should have 'psp:Context$binnenRolBeschrijving_buitenRol' as binding"
   --     (binding (RolInContext $ p "Context$rolInContext_1"))
   --     [RolDef (p "Context$binnenRolBeschrijving_buitenRol")]
   test "binding" do
-    assertEqual "Clustergenoot should have a Gebruiker as binding"
-      (binding (RolInContext $ u "MijnSysteem$MijnCluster$clusterGenoot_1"))
-      [RolInContext (u "MijnSysteem$gebruiker_1")]
-    assertEqual "Range of isVerplicht of Property should be Boolean"
-      (binding (RolInContext $ p "Property$buitenRolBeschrijving$isVerplicht$range_1"))
-      [RolInContext (p "Boolean_buitenRol")]
-    assertEqual "Context$rolInContext_1 should have 'psp:Context$binnenRolBeschrijving_buitenRol' as binding"
-      (binding (RolInContext $ p "Context$rolInContext_1"))
-      [RolInContext (p "Context$binnenRolBeschrijving_buitenRol")]
+    assertEqual "rolInContext of u:myContext should be bound to psp:Context_buitenRol"
+      (((iedereRolInContext >=> pure <<< map RolInContext) /-/ binding) (u "myContext"))
+      [RolInContext (p "Context_buitenRol")]
   test "context" do
     assertEqual "Context$rolInContext_1 should have 'psp:Context' as context"
       (context (RolInContext $ p "Context$rolInContext_1"))
       [p "Context"]
+    assertEqual "The role of u:myContext should have 'u:myContext' as context"
+      (((iedereRolInContext >=> pure <<< map RolInContext) /-/ context) (u "myContext"))
+      [u "myContext"]
   test "rolBindingDef" do
-    assertEqual "Context$rolInContext_1 should have 'psp:Context$binnenRolBeschrijving' as rolBindingDef"
-      (rolBindingDef (RolInContext $ p "Context$rolInContext_1"))
-      [p "Context$binnenRolBeschrijving"]
-  test "context" do
-    assertEqual "The instance of the rol Aspect called 'psp:Zaak$aspect_1' should have 'psp:Zaak' as context."
-      (context (ContextRol $ p "Zaak$aspect_1"))
-      [p "Zaak"]
-    assertEqual "The instance of the rol RolProperty called 'psp:Systeem$gebruiker$rolProperty_1' should have 'psp:Systeem$gebruiker' as context."
-      (context (ContextRol $ p "Systeem$gebruiker$rolProperty_1"))
-      [p "Systeem$gebruiker"]
+    assertEqual "The role of u:myContext should have 'psp:Context' as rolBindingDef"
+      ((iedereRolInContext >=> pure <<< map RolInContext) /-/ rolBindingDef $ (u "myContext"))
+      [p "Context"]
   test "Tearing down" do
     removeTestContext (u "myContext")
