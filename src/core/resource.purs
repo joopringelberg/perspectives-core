59ad4783db32f590855dc0e58b4643585c34f6c5
Joop Ringelberg <joop.ringelberg@gmail.com>
2019-05-03 15:23:24 +0200

    Resource

--- src/core/resource.purs
+++ src/core/resource.purs
@@ -2,9 +2,8 @@ module Perspectives.Resource where
 
 import Prelude
 
-import Effect.Aff.AVar (AVar, readVar)
+import Effect.Aff.AVar (AVar, read)
 import Effect.Aff.Class (liftAff)
-import Effect (kind Effect)
 import Control.Monad.Error.Class (catchError)
 import Data.Array (singleton)
 import Data.Maybe (Maybe(..), fromJust)
@@ -15,25 +14,25 @@ import Perspectives.EntiteitAndRDFAliases (ID)
 import Perspectives.PerspectEntiteit (class PerspectEntiteit, getType, removeInternally, retrieveInternally)
 import Perspectives.ResourceRetrieval (fetchPerspectEntiteitFromCouchdb)
 
-getPerspectEntiteit :: forall e a. PerspectEntiteit a => ID -> MonadPerspectives (AjaxAvarCache e) a
+getPerspectEntiteit :: forall a. PerspectEntiteit a => ID -> MonadPerspectives a
 getPerspectEntiteit id =
   do
     (av :: Maybe (AVar a)) <- retrieveInternally id
     case av of
       (Just avar) -> do
-        pe <- liftAff $ readVar avar
+        pe <- liftAff $ read avar
         pure pe
       Nothing -> do
         (ent :: a) <- fetchPerspectEntiteitFromCouchdb id
         pure ent
 
-tryGetPerspectEntiteit :: forall e a. PerspectEntiteit a => ID -> MonadPerspectives (AjaxAvarCache e) (Maybe a)
+tryGetPerspectEntiteit :: forall a. PerspectEntiteit a => ID -> MonadPerspectives (Maybe a)
 tryGetPerspectEntiteit id = catchError ((getPerspectEntiteit id) >>= (pure <<< Just))
   \_ -> do
     (_ :: Maybe (AVar a)) <- removeInternally id
     pure Nothing
 
-getAVarRepresentingPerspectEntiteit :: forall e a. PerspectEntiteit a => ID -> MonadPerspectives (AjaxAvarCache e) (AVar a)
+getAVarRepresentingPerspectEntiteit :: forall a. PerspectEntiteit a => ID -> MonadPerspectives (AVar a)
 getAVarRepresentingPerspectEntiteit id =
   do
     (av :: Maybe (AVar a)) <- retrieveInternally id
@@ -44,7 +43,7 @@ getAVarRepresentingPerspectEntiteit id =
         mavar <- retrieveInternally id
         pure $ unsafePartial $ fromJust mavar
 
-entiteitType :: forall e a. PerspectEntiteit a => ID -> MonadPerspectives (AjaxAvarCache e) (Array String)
+entiteitType :: forall a. PerspectEntiteit a => ID -> MonadPerspectives (Array String)
 entiteitType id = do
   (a :: a) <- getPerspectEntiteit id
   pure $ singleton $ getType a
