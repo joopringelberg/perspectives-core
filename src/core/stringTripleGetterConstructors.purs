bc6804fbdb1512e893bb795a801ade6b1af91348
Joop Ringelberg <joop.ringelberg@gmail.com>
2019-05-03 17:04:22 +0200

    StringTypedTripleGetters

--- src/core/stringTripleGetterConstructors.purs
+++ src/core/stringTripleGetterConstructors.purs
@@ -20,173 +20,173 @@ import Prelude (flip, (<>))
 -----------------------------------------------------------
 -- COMBINATORS
 -----------------------------------------------------------
-type StringTripleGetter e = TripleGetter String String e
-type StringTypedTripleGetter e = (String **> String) e
+type StringTripleGetter = TripleGetter String String
+type StringTypedTripleGetter = (String **> String)
 
-searchInPrototypeHierarchy :: forall e. StringTypedTripleGetter e -> StringTypedTripleGetter e
+searchInPrototypeHierarchy :: StringTypedTripleGetter -> StringTypedTripleGetter
 searchInPrototypeHierarchy getter = typeWithPerspectivesTypes DTG.buitenRol >-> TGC.searchInRolTelescope getter
 
-searchLocallyAndInPrototypeHierarchy :: forall e. StringTypedTripleGetter e -> StringTypedTripleGetter e
+searchLocallyAndInPrototypeHierarchy :: StringTypedTripleGetter -> StringTypedTripleGetter
 searchLocallyAndInPrototypeHierarchy getter@(TypedTripleGetter n _) = TypedTripleGetter n f where
-  f :: StringTripleGetter e
+  f :: StringTripleGetter
   f (cid :: String) =
     TGC.unlessNull getter cid
     <|>
     (cid @@ (DTG.buitenRol >-> DTG.binding >-> DTG.context >-> searchLocallyAndInPrototypeHierarchy getter))
 
-searchInAspectsAndPrototypes :: forall e. StringTypedTripleGetter e -> StringTypedTripleGetter e
+searchInAspectsAndPrototypes :: StringTypedTripleGetter -> StringTypedTripleGetter
 searchInAspectsAndPrototypes getter@(TypedTripleGetter n _) = TypedTripleGetter n f where
-  f :: StringTripleGetter e
+  f :: StringTripleGetter
   f contextId =
     TGC.unlessNull (searchLocallyAndInPrototypeHierarchy getter) contextId
     <|>
     (contextId @@ (TGC.directAspects >-> searchInAspectsAndPrototypes getter))
 
--- | Applies the StringTypedTripleGetter e to the RolDef and all its prototypes and recursively to all its aspects.
+-- | Applies the StringTypedTripleGetter to the RolDef and all its prototypes and recursively to all its aspects.
 -- Test.Perspectives.ModelBasedTripleGetters, via propertiesDef
-searchInAspectRolesAndPrototypes :: forall e. StringTypedTripleGetter e -> StringTypedTripleGetter e
+searchInAspectRolesAndPrototypes :: StringTypedTripleGetter -> StringTypedTripleGetter
 searchInAspectRolesAndPrototypes getter@(TypedTripleGetter n _) = TypedTripleGetter n f where
-  f :: StringTripleGetter e
+  f :: StringTripleGetter
   f rolDefId =
     TGC.unlessNull (searchLocallyAndInPrototypeHierarchy getter) rolDefId
     <|>
     (rolDefId @@ (RolDef `before` TGC.directAspectRoles >-> unwrap `before` searchInAspectRolesAndPrototypes getter))
 
-directAspectRoles :: forall e. StringTypedTripleGetter e
+directAspectRoles :: StringTypedTripleGetter
 directAspectRoles = typeWithPerspectivesTypes OGC.directAspectRoles `trackedAs` "model:Perspectives$Rol$directAspectRoles"
 
-directAspectProperties :: forall e. StringTypedTripleGetter e
+directAspectProperties :: StringTypedTripleGetter
 directAspectProperties = typeWithPerspectivesTypes OGC.directAspectProperties `trackedAs` "model:Perspectives$Property$directAspectProperties"
 
 -----------------------------------------------------------
 -- CLOSURES
 -----------------------------------------------------------
-closureOfBinding :: forall e. StringTypedTripleGetter e
+closureOfBinding :: StringTypedTripleGetter
 closureOfBinding = TGC.closure DTG.genericBinding
 
-closureOfAspectRol :: forall e. StringTypedTripleGetter e
+closureOfAspectRol :: StringTypedTripleGetter
 closureOfAspectRol = TGC.closure directAspectRoles
 
-closureOfAspectProperty :: forall e. StringTypedTripleGetter e
+closureOfAspectProperty :: StringTypedTripleGetter
 closureOfAspectProperty = TGC.closure directAspectProperties
 
 -----------------------------------------------------------
 -- GET A ROL FROM A CONTEXT
 -----------------------------------------------------------
-getContextRol :: forall e. String -> StringTypedTripleGetter e
+getContextRol :: String -> StringTypedTripleGetter
 getContextRol rn = typeWithPerspectivesTypes OGC.getContextRol rn `trackedAs` rn
 
-getRolInContext :: forall e. String -> StringTypedTripleGetter e
+getRolInContext :: String -> StringTypedTripleGetter
 getRolInContext = getContextRol
 
-getUnqualifiedContextRol :: forall e. Id.LocalName -> StringTypedTripleGetter e
+getUnqualifiedContextRol :: Id.LocalName -> StringTypedTripleGetter
 getUnqualifiedContextRol ln = typeWithPerspectivesTypes OGC.getUnqualifiedContextRol ln `trackedAs` ln
 
-getUnqualifiedRolInContext :: forall e. Id.LocalName -> StringTypedTripleGetter e
+getUnqualifiedRolInContext :: Id.LocalName -> StringTypedTripleGetter
 getUnqualifiedRolInContext = getUnqualifiedContextRol
 
-rolesOf :: forall e. String -> StringTypedTripleGetter e
+rolesOf :: String -> StringTypedTripleGetter
 rolesOf cid = TypedTripleGetter ("rolesOf_" <> cid) (typeWithPerspectivesTypes getter) where
-  getter :: String -> MPQ e (Triple String String e)
+  getter :: String -> MPQ (Triple String String)
   getter rd = cid @@ typeWithPerspectivesTypes TGC.searchRolInContext rd
 
 -----------------------------------------------------------
 -- SEARCH A ROL IN A CONTEXT AND ITS PROTOTYPES
 -----------------------------------------------------------
 -- | Search for a qualified ContextRol both in the local context and all its prototypes.
-searchContextRol :: forall e. String -> StringTypedTripleGetter e
+searchContextRol :: String -> StringTypedTripleGetter
 searchContextRol rn = searchLocallyAndInPrototypeHierarchy ((getContextRol rn))
 
-searchRolInContext :: forall e. String -> StringTypedTripleGetter e
+searchRolInContext :: String -> StringTypedTripleGetter
 searchRolInContext = searchContextRol
 
-searchUnqualifiedRol :: forall e. Id.LocalName -> StringTypedTripleGetter e
+searchUnqualifiedRol :: Id.LocalName -> StringTypedTripleGetter
 searchUnqualifiedRol rn = searchLocallyAndInPrototypeHierarchy (DTG.genericContext >-> (getUnqualifiedContextRol rn))
 
 -----------------------------------------------------------
 -- GET A ROLDEFINITION FROM A CONTEXT DEFINITION
 -----------------------------------------------------------
-getUnqualifiedRolDefinition ::	forall e. Id.LocalName -> StringTypedTripleGetter e
+getUnqualifiedRolDefinition :: Id.LocalName -> StringTypedTripleGetter
 getUnqualifiedRolDefinition ln = (QC.filter_
   (flip Id.hasLocalName ln)
   ("hasLocalName_" <> ln)
   (getUnqualifiedContextRol "rolInContext" >-> DTG.genericBinding >-> DTG.genericContext))
 
-searchUnqualifiedRolDefinition ::	forall e. Id.LocalName -> StringTypedTripleGetter e
+searchUnqualifiedRolDefinition :: Id.LocalName -> StringTypedTripleGetter
 searchUnqualifiedRolDefinition ln = searchInAspectsAndPrototypes f
   where
-    f :: (String **> String) e
+    f :: (String **> String)
     f = DTG.genericContext >-> getUnqualifiedRolDefinition ln
 
 -----------------------------------------------------------
 -- GET A PROPERTY FROM A ROLE TELESCOPE
 -----------------------------------------------------------
-searchProperty :: forall e. String -> StringTypedTripleGetter e
+searchProperty :: String -> StringTypedTripleGetter
 searchProperty pd = TGC.searchInRolTelescope g
   where
-    g :: (String **> String) e
+    g :: (String **> String)
     g = genericGetProperty pd `trackedAs` pd
 
-searchUnqualifiedProperty :: forall e. Id.LocalName -> StringTypedTripleGetter e
+searchUnqualifiedProperty :: Id.LocalName -> StringTypedTripleGetter
 searchUnqualifiedProperty pd = TGC.searchInRolTelescope g
   where
-    g :: StringTypedTripleGetter e
+    g :: StringTypedTripleGetter
     g = genericGetUnqualifiedLocalProperty pd `trackedAs` pd
 
 -----------------------------------------------------------
 -- GET A PROPERTY FROM A CONTEXT
 -----------------------------------------------------------
-searchPropertyOnContext :: forall e. StringTypedTripleGetter e -> String -> StringTypedTripleGetter e
+searchPropertyOnContext :: StringTypedTripleGetter -> String -> StringTypedTripleGetter
 searchPropertyOnContext rolgetter p = searchLocallyAndInPrototypeHierarchy f
   where
-    f :: StringTypedTripleGetter e
+    f :: StringTypedTripleGetter
     f = DTG.genericContext >-> rolgetter >-> searchProperty p
 
-searchUnqualifiedPropertyOnContext :: forall e. StringTypedTripleGetter e  -> Id.LocalName -> StringTypedTripleGetter e
+searchUnqualifiedPropertyOnContext :: StringTypedTripleGetter  -> Id.LocalName -> StringTypedTripleGetter
 searchUnqualifiedPropertyOnContext rolgetter p = searchLocallyAndInPrototypeHierarchy f
   where
-    f :: StringTypedTripleGetter e
+    f :: StringTypedTripleGetter
     f = DTG.genericContext >-> rolgetter >-> searchUnqualifiedProperty p
 
-searchExternalProperty :: forall e. String -> StringTypedTripleGetter e
+searchExternalProperty :: String -> StringTypedTripleGetter
 searchExternalProperty pn = searchPropertyOnContext (typeWithPerspectivesTypes DTG.buitenRol) pn
 
-searchExternalUnqualifiedProperty :: forall e. Id.LocalName -> StringTypedTripleGetter e
+searchExternalUnqualifiedProperty :: Id.LocalName -> StringTypedTripleGetter
 searchExternalUnqualifiedProperty ln = searchUnqualifiedPropertyOnContext (typeWithPerspectivesTypes DTG.buitenRol) ln
 
-getInternalProperty :: forall e. String -> StringTypedTripleGetter e
+getInternalProperty :: String -> StringTypedTripleGetter
 -- getInternalProperty pn = typeWithPerspectivesTypes DTG.binnenRol >-> genericGetProperty pn `trackedAs` pn
 getInternalProperty = typeWithPerspectivesTypes TGC.getInternalProperty
 
-searchInternalUnqualifiedProperty :: forall e. Id.LocalName -> StringTypedTripleGetter e
+searchInternalUnqualifiedProperty :: Id.LocalName -> StringTypedTripleGetter
 searchInternalUnqualifiedProperty ln = typeWithPerspectivesTypes DTG.binnenRol >-> searchUnqualifiedProperty ln
 
-getRoleBinders :: forall e. String -> StringTypedTripleGetter e
+getRoleBinders :: String -> StringTypedTripleGetter
 getRoleBinders rname = OGC.genericGetRoleBinders rname `trackedAs` rname
 
 -----------------------------------------------------------
 -- GET A PROPERTYDEFINITION FROM A ROL DEFINITION
 -----------------------------------------------------------
-getUnqualifiedPropertyDefinition ::	forall e. Id.LocalName -> StringTypedTripleGetter e
+getUnqualifiedPropertyDefinition :: Id.LocalName -> StringTypedTripleGetter
 getUnqualifiedPropertyDefinition ln = (QC.filter_
   (flip Id.hasLocalName ln)
   ("hasLocalName_" <> ln)
   (getUnqualifiedContextRol "rolInContext" >-> DTG.genericBinding >-> DTG.genericContext))
 
-searchUnqualifiedPropertyDefinition ::	forall e. Id.LocalName -> StringTypedTripleGetter e
+searchUnqualifiedPropertyDefinition :: Id.LocalName -> StringTypedTripleGetter
 searchUnqualifiedPropertyDefinition ln = searchInAspectsAndPrototypes f
   where
-    f :: StringTypedTripleGetter e
+    f :: StringTypedTripleGetter
     f = DTG.genericContext >-> getUnqualifiedPropertyDefinition ln
 
 -----------------------------------------------------------
 -- INVERSE ROL
 -----------------------------------------------------------
-constructInverseRolGetter :: forall e.
+constructInverseRolGetter ::
   String ->
-  StringTypedTripleGetter e
+  StringTypedTripleGetter
 constructInverseRolGetter pn = (OGC.genericGetRoleBinders pn) `trackedAs` (pn <> "_inverse")
 
 -- | The PropertyReferences of the View. Again, the typing is imprecise.
-propertyReferenties :: forall e. StringTypedTripleGetter e
+propertyReferenties :: StringTypedTripleGetter
 propertyReferenties = typeWithPerspectivesTypes searchUnqualifiedRolDefinition "propertyReferentie"
