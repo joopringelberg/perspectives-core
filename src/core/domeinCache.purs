8aabef75a82737e119ac97d3455238432936a1d8
Joop Ringelberg <joop.ringelberg@gmail.com>
2019-05-03 14:04:05 +0200

    DomeinCache

--- src/core/domeinCache.purs
+++ src/core/domeinCache.purs
@@ -3,56 +3,58 @@ module Perspectives.DomeinCache
 
 where
 
-import Effect.Aff (Aff, catchError)
-import Effect.Aff.AVar (AVar, makeEmptyVar, putVar, readVar, takeVar)
-import Effect.Aff.Class (liftAff)
-import Effect.Class (liftEff)
-import Effect.Exception (error)
-import Control.Monad.Except (throwError)
+import Affjax (Request, request, put) as AX
+import Affjax (ResponseFormatError, printResponseFormatError)
+import Affjax.RequestBody as RequestBody
+import Affjax.ResponseFormat as ResponseFormat
+import Affjax.StatusCode (StatusCode(..))
+import Control.Monad.Except (class MonadError, runExcept, throwError)
 import Data.Either (Either(..))
-import Foreign.Generic (encodeJSON)
 import Data.HTTP.Method (Method(..))
 import Data.Maybe (Maybe(..), fromJust)
 import Data.Newtype (unwrap)
+import Effect.Aff (Aff, catchError)
+import Effect.Aff.AVar (AVar, empty, put, read, take)
+import Effect.Aff.Class (liftAff)
+import Effect.Exception (error, Error)
+import Foreign (MultipleErrors)
+import Foreign.Class (class Decode)
+import Foreign.Generic (decodeJSON, encodeJSON)
 import Foreign.Object (lookup)
-import Affjax (AJAX, AffjaxRequest, AffjaxResponse, affjax, put)
-import Affjax.StatusCode (StatusCode(..))
 import Partial.Unsafe (unsafePartial)
 import Perspectives.CoreTypes (MonadPerspectives)
-import Perspectives.Couchdb (DeleteCouchdbDocument(..), DocReference(..), GetCouchdbAllDocs(..), PutCouchdbDocument, onAccepted)
+import Perspectives.Couchdb (DocReference(..), GetCouchdbAllDocs(..), PutCouchdbDocument, onAccepted)
 import Perspectives.Couchdb.Databases (defaultPerspectRequest, retrieveDocumentVersion)
 import Perspectives.DomeinFile (DomeinFile(..))
-
 import Perspectives.EntiteitAndRDFAliases (ContextID, RolID, ID)
-import Perspectives.GlobalUnsafeStrMap (poke)
 import Perspectives.Identifiers (Namespace, escapeCouchdbDocumentName)
-import Perspectives.PerspectivesState (domeinCache, domeinCacheInsert, domeinCacheLookup, domeinCacheRemove)
+import Perspectives.PerspectivesState (domeinCacheInsert, domeinCacheLookup, domeinCacheRemove)
 import Perspectives.Syntax (PerspectContext, PerspectRol, revision)
-import Prelude (Unit, bind, discard, pure, show, unit, ($), (*>), (<$>), (<>), (==), (>>=))
+import Prelude (Unit, bind, discard, pure, show, unit, void, ($), (*>), (<$>), (<>), (==), (>>=))
 
 type URL = String
 
-storeDomeinFileInCache :: forall e. Namespace -> DomeinFile -> MonadPerspectives (AvarCache e) (AVar DomeinFile)
+storeDomeinFileInCache :: Namespace -> DomeinFile -> MonadPerspectives (AVar DomeinFile)
 storeDomeinFileInCache ns df= do
-  ev <- (liftAff makeEmptyVar) >>= domeinCacheInsert ns
-  liftAff $ putVar df ev
+  ev <- (liftAff empty) >>= domeinCacheInsert ns
+  liftAff $ put df ev
   pure ev
 
 -- | Change the domeinfile in cache. NOTA BENE: does not store the modified file in Couchdb!
-modifyDomeinFileInCache :: forall e. Namespace -> (DomeinFile -> DomeinFile) -> MonadPerspectives (AvarCache e) Unit
+modifyDomeinFileInCache :: Namespace -> (DomeinFile -> DomeinFile) -> MonadPerspectives Unit
 modifyDomeinFileInCache ns modifier = do
   mAvar <- domeinCacheLookup ns
   case mAvar of
     Nothing -> throwError $ error $ "modifyDomeinFileInCache cannot find domeinfile in cache: " <> ns
     (Just avar) -> do
-      df <- liftAff $ takeVar avar
-      liftAff $ putVar (modifier df) avar
+      df <- liftAff $ take avar
+      liftAff $ put (modifier df) avar
 
 -- | Fetch a PerspectContext asynchronously from its Domein, loading the Domein file if necessary.
-retrieveContextFromDomein :: forall e.
+retrieveContextFromDomein ::
   ContextID
   -> Namespace
-  -> (MonadPerspectives (AjaxAvarCache e) PerspectContext)
+  -> (MonadPerspectives PerspectContext)
 retrieveContextFromDomein id ns = do
   (DomeinFile {contexts}) <- retrieveDomeinFile ns
   case lookup id contexts of
@@ -60,103 +62,114 @@ retrieveContextFromDomein id ns = do
     (Just context) -> pure context
 
 -- | Fetch a PerspectRol asynchronously from its Domein, loading the Domein file if necessary.
-retrieveRolFromDomein :: forall e.
+retrieveRolFromDomein ::
   RolID
   -> Namespace
-  -> (MonadPerspectives (AjaxAvarCache e) PerspectRol)
+  -> (MonadPerspectives PerspectRol)
 retrieveRolFromDomein id ns = do
   (DomeinFile {roles}) <- retrieveDomeinFile ns
   case lookup id roles of
     Nothing -> throwError $ error ("retrieveRolFromDomein: cannot find definition of " <> id <> " in retrieveRolFromDomein for " <> ns)
     (Just rol) -> pure rol
 
-retrieveDomeinFile :: forall e. Namespace -> MonadPerspectives (AjaxAvarCache e) DomeinFile
+retrieveDomeinFile :: Namespace -> MonadPerspectives DomeinFile
 retrieveDomeinFile ns = do
   mAvar <- domeinCacheLookup ns
   case mAvar of
     Nothing -> do
-      ev <- (liftAff makeEmptyVar) >>= domeinCacheInsert ns
+      ev <- (liftAff empty) >>= domeinCacheInsert ns
       -- forkAff hinders catchError.
       -- _ <- forkAff do
       res <- catchError
-        (liftAff $ affjax $ domeinRequest {url = modelsURL <> escapeCouchdbDocumentName ns})
+        (liftAff $ AX.request $ domeinRequest {url = modelsURL <> escapeCouchdbDocumentName ns})
         \e -> throwError $ error $ "Failure in retrieveDomeinFile for: " <> ns <> ". " <> show e
-      liftAff $ onAccepted res.status [200, 304] "retrieveDomeinFile" $ putVar res.response ev
-      liftAff $ readVar ev
-    (Just avar) -> liftAff $ readVar avar
+      onAccepted res.status [200, 304] "retrieveDomeinFile"
+        (onCorrectCallAndResponse res.body (\(a :: DomeinFile) -> liftAff $ put a ev))
+    (Just avar) -> liftAff $ read avar
 
 -- | A name not preceded or followed by a forward slash.
 type DatabaseName = String
 
-documentsInDatabase :: forall e. DatabaseName -> Aff (ajax :: AJAX | e) GetCouchdbAllDocs
+documentsInDatabase :: DatabaseName -> Aff GetCouchdbAllDocs
 documentsInDatabase database = do
-  res <- affjax $ domeinRequest {url = baseURL <> escapeCouchdbDocumentName database <> "/_all_docs"}
+  res <- AX.request $ domeinRequest {url = baseURL <> escapeCouchdbDocumentName database <> "/_all_docs"}
   onAccepted res.status [200] "documentsInDatabase"
-    $ pure res.response
+    (onCorrectCallAndResponse res.body \(a :: GetCouchdbAllDocs) -> pure unit)
 
-documentNamesInDatabase :: forall e. DatabaseName -> Aff (ajax :: AJAX | e) (Array String)
+documentNamesInDatabase :: DatabaseName -> Aff (Array String)
 documentNamesInDatabase database = do
   (GetCouchdbAllDocs cad) <- documentsInDatabase database
   pure $ (\(DocReference{id}) -> id) <$> cad.rows
 
-saveCachedDomeinFile :: forall e. ID -> MonadPerspectives (AjaxAvarCache e) Unit
+saveCachedDomeinFile :: ID -> MonadPerspectives Unit
 saveCachedDomeinFile ns = do
   mAvar <- domeinCacheLookup ns
   case mAvar of
     Nothing -> throwError $ error $ "saveCachedDomeinFile: cannot find domeinfile in cache: " <> ns
     (Just avar) -> do
-      df <- liftAff $ readVar avar
+      df <- liftAff $ read avar
       modifyDomeinFileInCouchdb df avar
 
 -- | Either create or modify the DomeinFile in couchdb. Do not use createDomeinFileInCouchdb or modifyDomeinFileInCouchdb directly.
-storeDomeinFileInCouchdb :: forall e. DomeinFile -> MonadPerspectives (AjaxAvarCache e) Unit
+storeDomeinFileInCouchdb :: DomeinFile -> MonadPerspectives Unit
 storeDomeinFileInCouchdb df@(DomeinFile {_id}) = do
   mAvar <- domeinCacheLookup _id
-  -- mAvar <- liftEff $ peek domeinCache _id
+  -- mAvar <- liftEffect $ peek domeinCache _id
   case mAvar of
     Nothing -> createDomeinFileInCouchdb df
     (Just avar) -> modifyDomeinFileInCouchdb df avar
 
-createDomeinFileInCouchdb :: forall e. DomeinFile -> MonadPerspectives (AjaxAvarCache e) Unit
+createDomeinFileInCouchdb :: DomeinFile -> MonadPerspectives Unit
 createDomeinFileInCouchdb df@(DomeinFile dfr@{_id, contexts}) = do
-  ev <- (liftAff makeEmptyVar) >>= domeinCacheInsert _id
-  (res :: AffjaxResponse PutCouchdbDocument)  <- liftAff $ put (modelsURL <> escapeCouchdbDocumentName _id) (encodeJSON df)
+  ev <- (liftAff empty) >>= domeinCacheInsert _id
+  res <- liftAff $ AX.put ResponseFormat.string (modelsURL <> escapeCouchdbDocumentName _id) (RequestBody.string (encodeJSON df))
   if res.status == (StatusCode 409)
     then do
       rev <- retrieveDocumentVersion (modelsURL <> escapeCouchdbDocumentName _id)
       setRevision rev ev
-      updatedDomeinFile <- liftAff $ readVar ev
+      updatedDomeinFile <- liftAff $ read ev
       modifyDomeinFileInCouchdb updatedDomeinFile ev
-    else onAccepted res.status [200, 201] "createDomeinFileInCouchdb" $ setRevision (unsafePartial $ fromJust $ (unwrap res.response).rev) ev
+    else onAccepted res.status [200, 201] "createDomeinFileInCouchdb"
+      (void $ onCorrectCallAndResponse res.body \(a :: PutCouchdbDocument) -> setRevision (unsafePartial $ fromJust $ (unwrap a).rev) ev)
   where
-    setRevision :: String -> (AVar DomeinFile) -> MonadPerspectives (AjaxAvarCache e) Unit
-    setRevision s av = liftAff $ putVar (DomeinFile (dfr {_rev = (revision s)})) av
+    setRevision :: String -> (AVar DomeinFile) -> MonadPerspectives Unit
+    setRevision s av = liftAff $ put (DomeinFile (dfr {_rev = (revision s)})) av
 
-modifyDomeinFileInCouchdb :: forall e. DomeinFile -> (AVar DomeinFile) -> MonadPerspectives (AjaxAvarCache e) Unit
+onCorrectCallAndResponse :: forall a m. MonadError Error m => Decode a => Either ResponseFormatError String -> (a -> m Unit) -> m a
+onCorrectCallAndResponse (Left e) _ = throwError $ error ("createDomeinFileInCouchdb: error in call: " <> printResponseFormatError e)
+onCorrectCallAndResponse (Right r) f = do
+  (x :: Either MultipleErrors a) <- pure $ runExcept (decodeJSON r)
+  case x of
+    (Left e) -> throwError $ error ("createDomeinFileInCouchdb: error in decoding result: " <> show e)
+    (Right result) -> f result *> pure result
+
+modifyDomeinFileInCouchdb :: DomeinFile -> (AVar DomeinFile) -> MonadPerspectives Unit
 modifyDomeinFileInCouchdb df@(DomeinFile dfr@{_id}) av = do
-  (DomeinFile {_rev}) <- liftAff $ readVar av
+  (DomeinFile {_rev}) <- liftAff $ read av
   originalRevision <- pure $ unsafePartial $ fromJust _rev
-  oldDf <- liftAff $ takeVar av
-  (res :: AffjaxResponse PutCouchdbDocument) <- liftAff $ put
+  oldDf <- liftAff $ take av
+  res <- liftAff $ AX.put
+    ResponseFormat.string
     (modelsURL <> escapeCouchdbDocumentName _id <> "?_rev=" <> originalRevision)
-    (encodeJSON (DomeinFile dfr {_rev = _rev}))
+    (RequestBody.string (encodeJSON (DomeinFile dfr {_rev = _rev})))
   if res.status == (StatusCode 409)
     then do
       rev <- retrieveDocumentVersion (modelsURL <> escapeCouchdbDocumentName _id)
       setRevision rev
-      updatedDomeinFile <- liftAff $ readVar av
+      updatedDomeinFile <- liftAff $ read av
       modifyDomeinFileInCouchdb updatedDomeinFile av
-    else onAccepted res.status [200, 201] "modifyDomeinFileInCouchdb" $ setRevision (unsafePartial $ fromJust $ (unwrap res.response).rev)
+    else onAccepted res.status [200, 201] "modifyDomeinFileInCouchdb"
+      (void (onCorrectCallAndResponse res.body \(a :: PutCouchdbDocument)-> setRevision (unsafePartial $ fromJust $ (unwrap a).rev)))
   where
-    setRevision :: String -> MonadPerspectives (AjaxAvarCache e) Unit
-    setRevision s = liftAff $ putVar (DomeinFile (dfr {_rev = (revision s)})) av
+    setRevision :: String -> MonadPerspectives Unit
+    setRevision s = liftAff $ put (DomeinFile (dfr {_rev = (revision s)})) av
 
 -- | Remove the file from couchb. Removes the model from cache.
-removeDomeinFileFromCouchdb :: forall e. Namespace -> MonadPerspectives (AjaxAvarCache e) Unit
+removeDomeinFileFromCouchdb :: Namespace -> MonadPerspectives Unit
 removeDomeinFileFromCouchdb ns = do
   rev <- retrieveDocumentVersion (modelsURL <> escapeCouchdbDocumentName ns)
-  (rq :: (AffjaxRequest Unit)) <- defaultPerspectRequest
-  (res :: AffjaxResponse DeleteCouchdbDocument) <- liftAff $ affjax $ rq {method = Left DELETE, url = (modelsURL <> escapeCouchdbDocumentName ns <> "?rev=" <> rev)}
+  (rq :: (AX.Request String)) <- defaultPerspectRequest
+  res <- liftAff $ AX.request $ rq {method = Left DELETE, url = (modelsURL <> escapeCouchdbDocumentName ns <> "?rev=" <> rev)}
   onAccepted res.status [200, 202] "removeDomeinFileFromCouchdb" $ domeinCacheRemove ns *> pure unit
 
 modelsURL :: URL
@@ -165,7 +178,7 @@ modelsURL = "http://localhost:5984/perspect_models/"
 baseURL :: URL
 baseURL = "http://localhost:5984/"
 
-domeinRequest :: AffjaxRequest Unit
+domeinRequest :: AX.Request String
 domeinRequest =
   { method: Left GET
   , url: "http://localhost:5984/models2model_SysteemDomein_"
@@ -174,4 +187,5 @@ domeinRequest =
   , username: Just "cor"
   , password: Just "geheim"
   , withCredentials: true
+  , responseFormat: ResponseFormat.string
   }
