2e3e12eb0bc1e9af0d7fad3d3ec531fc35711fec
Joop Ringelberg <joop.ringelberg@gmail.com>
2019-05-03 15:40:15 +0200

    ObjectsGetterComposition

--- src/core/ObjectsGetterComposition.purs
+++ src/core/ObjectsGetterComposition.purs
@@ -3,20 +3,20 @@ module Perspectives.ObjectsGetterComposition where
 import Data.Array (foldM, foldMap, intersect, singleton, union)
 import Data.Monoid.Disj (Disj(..))
 import Data.Newtype (alaF)
-import Perspectives.CoreTypes (ObjectsGetter, MP, type (~~>))
+import Perspectives.CoreTypes (MP, type (~~>))
 import Perspectives.PerspectivesTypes (PBool(..))
 import Prelude (class Eq, pure, show, (<<<), (==), (>=>), (>>=), (>>>))
 
-unionOfObjects :: forall s o t e.
+unionOfObjects :: forall s o t.
   Eq t =>
-  (s ~~> o) e ->
-  (o ~~> t) e ->
-  (s ~~> t) e
+  (s ~~> o) ->
+  (o ~~> t) ->
+  (s ~~> t)
 unionOfObjects p q = p >=> applyToAll q
 
-applyToAll :: forall o t e. Eq t =>
-  (o ~~> t) e ->
-  (Array o -> MP e (Array t))
+applyToAll :: forall o t. Eq t =>
+  (o ~~> t) ->
+  (Array o -> MP (Array t))
 applyToAll q objectsOfP = foldM
   (\(r :: Array t) (objectOfP :: o) -> q objectOfP >>= pure <<< union r)
   []
@@ -24,11 +24,11 @@ applyToAll q objectsOfP = foldM
 
 infixl 9 unionOfObjects as /-/
 
-intersectionOfObjects :: forall s o t e.
+intersectionOfObjects :: forall s o t.
   Eq t =>
-  (s ~~> o) e ->
-  (o ~~> t) e ->
-  (s ~~> t) e
+  (s ~~> o) ->
+  (o ~~> t) ->
+  (s ~~> t)
 intersectionOfObjects p q = p >=>
   (\objectsOfP -> foldM
       (\r objectOfP -> q objectOfP >>= pure <<< intersect r)
@@ -40,11 +40,11 @@ infixl 9 intersectionOfObjects as \-\
 -- | Compose an ObjectsGetter from an ObjectsGetter and a function
 -- | that maps an Array o value to an aggregated value.
 -- | This function typically folds over a monoid.
-composeMonoidal :: forall s o e a.
-  (s ~~> o) e
+composeMonoidal :: forall s o a.
+  (s ~~> o)
   -> (Array o -> a)
-  -> (s ~~> a) e
+  -> (s ~~> a)
 composeMonoidal p f = p >=> pure <<< singleton <<< f
 
-contains :: forall s o e. Eq o => o -> (s ~~> o) e -> (s ~~> PBool) e
+contains :: forall s o. Eq o => o -> (s ~~> o) -> (s ~~> PBool)
 contains obj p = p `composeMonoidal` (alaF Disj foldMap ((==) obj) >>> show >>> PBool)
