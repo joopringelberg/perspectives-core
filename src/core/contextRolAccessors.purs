9354104ec9c9ee05d127786a6dd763bb1c59adfa
Joop Ringelberg <joop.ringelberg@gmail.com>
2019-05-03 15:24:23 +0200

    ContextRolAccessors

--- src/core/contextRolAccessors.purs
+++ src/core/contextRolAccessors.purs
@@ -11,12 +11,12 @@ import Prelude (pure, (>=>), (<<<), (>>=))
 
 -- Can we safely assume that the PerspectContext will exist? If it does not because there is no model holding it,
 -- this function will break.
-getContextMember :: forall e. (PerspectContext -> Array String) -> ObjectsGetter e
+getContextMember :: (PerspectContext -> Array String) -> ObjectsGetter
 getContextMember f = getPerspectEntiteit >=> pure <<< f
 
 -- Even though members of a context will always be present, the context itself may not. Hence we return a Maybe value.
-getContextMember' :: forall a e. (PerspectContext -> a) -> (ID -> MonadPerspectives (AjaxAvarCache e) (Maybe a))
+getContextMember' :: forall a. (PerspectContext -> a) -> (ID -> MonadPerspectives (Maybe a))
 getContextMember' f c = catchError (getPerspectEntiteit c >>= pure <<< Just <<< f) \_ -> pure Nothing
 
-getRolMember :: forall e. (PerspectRol -> Array String) -> ObjectsGetter e
+getRolMember :: (PerspectRol -> Array String) -> ObjectsGetter
 getRolMember f = getPerspectEntiteit >=> pure <<< f
