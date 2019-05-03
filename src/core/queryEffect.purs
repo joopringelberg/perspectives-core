0a87ea9d56b7d9d520e174dc9471dd684a38f03e
Joop Ringelberg <joop.ringelberg@gmail.com>
2019-05-03 15:52:33 +0200

    QueryEffect

--- src/core/queryEffect.purs
+++ src/core/queryEffect.purs
@@ -1,30 +1,30 @@
 module Perspectives.QueryEffect where
 
-import Effect (Eff)
-import Effect.Class (liftEff)
+import Effect (Effect)
+import Effect.Class (liftEffect)
 import Perspectives.CoreTypes (NamedFunction(..), Triple(..), TripleGetter, TypedTripleGetter(..), MonadPerspectivesQuery, type (**>))
 
 import Perspectives.PerspectivesTypes (typeWithPerspectivesTypes)
 import Perspectives.TripleAdministration (getRef, registerTriple)
 import Prelude (Unit, bind, const, pure, ($), (<>))
 
-type QueryEffect e = NamedFunction (Array String -> Eff (AjaxAvarCache e) Unit)
+type QueryEffect = NamedFunction (Array String -> Effect Unit)
 
 -- | Make an effect function (QueryEffect) dependent on the objects of a TypedTripleGetter.
 -- | Results in a TypedTripleGetter.
 -- | Remove the effect function's dependency on the tripleGetter by using unsubscribeFromObjects.
-pushesObjectsTo :: forall s o e.
-  (s **> o) e ->
-  QueryEffect e ->
-  (s **> o) e
+pushesObjectsTo :: forall s o.
+  (s **> o) ->
+  QueryEffect ->
+  (s **> o)
 pushesObjectsTo (TypedTripleGetter tgName tg) (NamedFunction effectName effect) =
   TypedTripleGetter effectName pushesObjectsTo' where
 
-    pushesObjectsTo' :: TripleGetter s o e
+    pushesObjectsTo' :: TripleGetter s o
     pushesObjectsTo' id = do
       t <- tg id
       et <- effectFun t
-      _ <- liftEff $ registerTriple (typeWithPerspectivesTypes et)
+      _ <- liftEffect $ registerTriple (typeWithPerspectivesTypes et)
       pure et
       -- To unsubscribe the effect, de-register the effect triple.
 
@@ -32,9 +32,9 @@ pushesObjectsTo (TypedTripleGetter tgName tg) (NamedFunction effectName effect) 
     -- i.e. to sort the effect again and it will use the resulting triple to
     --  - set new dependencies based on its supports;
     --  - copy its supports to the triple administration (in the old effect triple)
-    effectFun :: Triple s o e -> MonadPerspectivesQuery (AjaxAvarCache e) (Triple s o e)
+    effectFun :: Triple s o -> MonadPerspectivesQuery (Triple s o)
     effectFun queryResult@(Triple{subject, object}) = do
-      _ <- liftEff $ effect (typeWithPerspectivesTypes object)
+      _ <- liftEffect $ effect (typeWithPerspectivesTypes object)
       pure $ Triple { subject: subject
                     , predicate : name
                     , object : object
