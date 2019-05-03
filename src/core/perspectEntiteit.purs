eddbccae5df5c3c9c4fe5aa9e311e6c34bc69d88
Joop Ringelberg <joop.ringelberg@gmail.com>
2019-05-03 14:15:35 +0200

    PerspectEntiteit

--- src/core/perspectEntiteit.purs
+++ src/core/perspectEntiteit.purs
@@ -1,26 +1,25 @@
 module Perspectives.PerspectEntiteit where
 
-import Effect.Aff.AVar (AVar, isEmptyVar, makeEmptyVar, putVar, readVar, takeVar)
-import Effect.Aff.Class (liftAff)
-import Effect.Exception (error)
 import Control.Monad.Except (throwError, runExcept)
 import Data.Either (Either)
+import Data.Maybe (Maybe(..))
+import Effect.Aff.AVar (AVar, isEmpty, empty, put, read, status, take)
+import Effect.Aff.Class (liftAff)
+import Effect.Exception (error)
 import Foreign (MultipleErrors)
 import Foreign.Class (class Encode, class Decode)
 import Foreign.Generic (decodeJSON, encodeJSON)
-import Data.Maybe (Maybe(..))
 import Perspectives.ContextAndRole (changeContext_rev, changeContext_rev', changeContext_type, changeRol_rev, changeRol_rev', changeRol_type, context_id, context_pspType, context_rev', rol_id, rol_pspType, rol_rev')
 import Perspectives.CoreTypes (MonadPerspectives)
 import Perspectives.DomeinCache (modifyDomeinFileInCache, retrieveContextFromDomein, retrieveRolFromDomein)
 import Perspectives.DomeinFile (addContextToDomeinFile, addRolToDomeinFile)
-
 import Perspectives.EntiteitAndRDFAliases (ID)
 import Perspectives.Identifiers (Namespace)
 import Perspectives.PerspectivesState (contextDefinitionsInsert, contextDefinitionsLookup, contextDefinitionsRemove, rolDefinitionsInsert, rolDefinitionsLookup, rolDefinitionsRemove)
 import Perspectives.Syntax (PerspectContext, PerspectRol, Revision)
-import Prelude (Unit, bind, discard, pure, unit, void, ($), (*>), (<<<), (<>), (>>=))
+import Prelude (Unit, bind, discard, pure, unit, void, ($), (*>), (<<<), (<>), (>>=), (>=>))
 
-class (Encode a, Decode a, Respondable a) <=  PerspectEntiteit a where
+class (Encode a, Decode a) <=  PerspectEntiteit a where
   getRevision' :: a -> Revision
   setRevision' :: Revision -> a -> a
   setRevision :: String -> a -> a
@@ -28,15 +27,15 @@ class (Encode a, Decode a, Respondable a) <=  PerspectEntiteit a where
   setType :: ID -> a -> a
   getId :: a -> ID
   -- | Create an empty AVar that will be filled by the PerspectEntiteit.
-  representInternally :: forall e. ID -> MonadPerspectives (AvarCache e) (AVar a)
-  retrieveInternally :: forall e. ID -> MonadPerspectives (AvarCache e) (Maybe (AVar a))
-  removeInternally :: forall e. ID -> MonadPerspectives (AvarCache e) (Maybe (AVar a))
+  representInternally :: ID -> MonadPerspectives (AVar a)
+  retrieveInternally :: ID -> MonadPerspectives (Maybe (AVar a))
+  removeInternally :: ID -> MonadPerspectives (Maybe (AVar a))
   -- | A default implementation for encode is encodeJSON.
   encode :: a -> String
   -- | A default implementation for decode is decodeJSON.
   decode :: String -> Either MultipleErrors a
-  retrieveFromDomein :: forall e. ID -> Namespace -> MonadPerspectives (AjaxAvarCache e) a
-  cacheInDomeinFile :: forall e. ID -> a -> MonadPerspectives (AvarCache e) Unit
+  retrieveFromDomein :: ID -> Namespace -> MonadPerspectives a
+  cacheInDomeinFile :: ID -> a -> MonadPerspectives Unit
 
 instance perspectEntiteitContext :: PerspectEntiteit PerspectContext where
   getRevision' = context_rev'
@@ -45,7 +44,7 @@ instance perspectEntiteitContext :: PerspectEntiteit PerspectContext where
   getType = context_pspType
   setType = changeContext_type
   getId = context_id
-  representInternally c = (liftAff makeEmptyVar) >>= contextDefinitionsInsert c
+  representInternally c = (liftAff empty) >>= contextDefinitionsInsert c
   retrieveInternally = contextDefinitionsLookup
   removeInternally = contextDefinitionsRemove
   encode = encodeJSON
@@ -60,7 +59,7 @@ instance perspectEntiteitRol :: PerspectEntiteit PerspectRol where
   getType = rol_pspType
   setType = changeRol_type
   getId = rol_id
-  representInternally c = (liftAff makeEmptyVar) >>= rolDefinitionsInsert c
+  representInternally c = (liftAff empty) >>= rolDefinitionsInsert c
   retrieveInternally = rolDefinitionsLookup
   removeInternally = rolDefinitionsRemove
   encode = encodeJSON
@@ -68,7 +67,7 @@ instance perspectEntiteitRol :: PerspectEntiteit PerspectRol where
   retrieveFromDomein = retrieveRolFromDomein
   cacheInDomeinFile ns c = modifyDomeinFileInCache ns (addRolToDomeinFile c)
 
-ensureInternalRepresentation :: forall e a. PerspectEntiteit a => ID -> MonadPerspectives (AvarCache e) (AVar a)
+ensureInternalRepresentation :: forall a. PerspectEntiteit a => ID -> MonadPerspectives (AVar a)
 ensureInternalRepresentation c = do
     mav <- retrieveInternally c
     case mav of
@@ -77,19 +76,19 @@ ensureInternalRepresentation c = do
 
 -- | Caches the entiteit. If it was cached before, ensures that the newly cached
 -- | entiteit has the same revision value as the old one.
-cacheEntiteitPreservingVersion :: forall e a. PerspectEntiteit a => ID -> a -> MonadPerspectives (AvarCache e) Unit
+cacheEntiteitPreservingVersion :: forall a. PerspectEntiteit a => ID -> a -> MonadPerspectives Unit
 cacheEntiteitPreservingVersion id e = do
   (mAvar :: Maybe (AVar a)) <- retrieveInternally id
   case mAvar of
     Nothing -> cacheUncachedEntiteit id e
     (Just avar) -> do
-      ent <- liftAff $ takeVar avar
+      ent <- liftAff $ take avar
       e' <- pure $ setRevision' (getRevision' ent) e
-      liftAff $ putVar e' avar
+      liftAff $ put e' avar
 
 -- | If the entiteit is represented in an AVar, overwrites the stored value.
 -- | Otherwise adds an AVar and stores the entiteit in it.
-cacheEntiteit :: forall e a. PerspectEntiteit a => ID -> a -> MonadPerspectives (AvarCache e) Unit
+cacheEntiteit :: forall a. PerspectEntiteit a => ID -> a -> MonadPerspectives Unit
 cacheEntiteit id e = do
   (mAvar :: Maybe (AVar a)) <- retrieveInternally id
   case mAvar of
@@ -97,39 +96,39 @@ cacheEntiteit id e = do
     otherwise -> void $ cacheCachedEntiteit id e
 
 -- | Store an internally created PerspectEntiteit for the first time in the local store.
-cacheUncachedEntiteit :: forall e a. PerspectEntiteit a => ID -> a -> MonadPerspectives (AvarCache e) Unit
+cacheUncachedEntiteit :: forall a. PerspectEntiteit a => ID -> a -> MonadPerspectives Unit
 cacheUncachedEntiteit id e = do
   (mAvar :: Maybe (AVar a)) <- retrieveInternally id
   case mAvar of
     Nothing -> do
       (av :: AVar a) <- representInternally id
-      liftAff $ putVar e av
+      liftAff $ put e av
       pure unit
     otherwise -> throwError $ error $ "cacheUncachedEntiteit: the cache should not hold an AVar for " <> id
 
 -- | Modify a PerspectEntiteit in the cache.
-cacheCachedEntiteit :: forall e a. PerspectEntiteit a => ID -> a -> MonadPerspectives (AvarCache e) a
+cacheCachedEntiteit :: forall a. PerspectEntiteit a => ID -> a -> MonadPerspectives a
 cacheCachedEntiteit id e = do
   mAvar <- retrieveInternally id
   case mAvar of
     Nothing -> throwError $ error $ "cacheCachedEntiteit: cannot change an entiteit that is not cached: " <> id
     (Just avar) -> do
-      empty <- liftAff $ isEmptyVar avar
+      empty <- liftAff $ (status >=> pure <<< isEmpty) avar
       if empty
-        then liftAff $ putVar e avar *> pure e
+        then liftAff $ put e avar *> pure e
         else do
-          _ <- liftAff $ takeVar avar
-          liftAff $ putVar e avar *> pure e
+          _ <- liftAff $ take avar
+          liftAff $ put e avar *> pure e
 
 -- | Returns an entity. Throws an error if the resource is not represented in cache or not
 -- | immediately available in cache.
-readEntiteitFromCache :: forall e a. PerspectEntiteit a => ID -> MonadPerspectives (AvarCache e) a
+readEntiteitFromCache :: forall a. PerspectEntiteit a => ID -> MonadPerspectives a
 readEntiteitFromCache id = do
   (mAvar :: Maybe (AVar a)) <- retrieveInternally id
   case mAvar of
     Nothing -> throwError $ error ("readEntiteitFromCache needs a locally stored resource for " <> id)
     (Just avar) -> do
-      empty <- liftAff $ isEmptyVar avar
+      empty <- liftAff $ (status >=> pure <<< isEmpty) avar
       if empty
         then throwError $ error ("readEntiteitFromCache found an empty AVar for " <> id)
-        else liftAff $ readVar avar
+        else liftAff $ read avar
