30bcf7854bc5f4a7cc2319410fb8ea87797fdcdb
Joop Ringelberg <joop.ringelberg@gmail.com>
2019-05-03 15:41:40 +0200

    QueryCache

--- src/core/queryCache.purs
+++ src/core/queryCache.purs
@@ -1,20 +1,20 @@
 module Perspectives.QueryCache where
 
-import Effect.Class (liftEff)
+import Effect.Class (liftEffect)
 import Control.Monad.Trans.Class (lift)
 import Data.Maybe (Maybe)
 import Perspectives.CoreTypes (MonadPerspectives, QueryCache, type (**>))
-import Perspectives.GlobalUnsafeStrMap (GLOBALMAP, new, peek, poke)
+import Perspectives.GlobalUnsafeStrMap (new, peek, poke)
 import Perspectives.PerspectivesTypes (typeWithPerspectivesTypes)
 import Prelude (pure, unit, ($), bind)
 
-queryCache :: forall e. (QueryCache e)
+queryCache :: QueryCache
 queryCache = new unit
 
-queryCacheInsert :: forall s o e1 e2. String -> (s **> o) e1 -> MonadPerspectives (gm :: GLOBALMAP | e2) ((s **> o) e1)
+queryCacheInsert :: forall s o. String -> (s **> o) -> MonadPerspectives ((s **> o))
 queryCacheInsert qname getter = do
-  _ <- lift $ liftEff $ poke queryCache qname (typeWithPerspectivesTypes getter)
+  _ <- lift $ liftEffect $ poke queryCache qname (typeWithPerspectivesTypes getter)
   pure getter
 
-queryCacheLookup :: forall s o e1 e2. String -> MonadPerspectives (gm :: GLOBALMAP | e1) (Maybe ((s **> o) e2))
-queryCacheLookup qname = liftEff $ typeWithPerspectivesTypes $ peek queryCache qname
+queryCacheLookup :: forall s o. String -> MonadPerspectives (Maybe ((s **> o)))
+queryCacheLookup qname = liftEffect $ typeWithPerspectivesTypes $ peek queryCache qname
