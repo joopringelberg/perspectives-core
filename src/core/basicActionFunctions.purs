36659abc5e458e4b36c6323f85833cf162fda828
Joop Ringelberg <joop.ringelberg@gmail.com>
2019-07-21 16:17:16 +0200

    basicActionFunctions

--- src/core/basicActionFunctions.purs
+++ src/core/basicActionFunctions.purs
@@ -3,23 +3,24 @@ module Perspectives.BasicActionFunctions where
 import Prelude
 
 import Data.Maybe (Maybe(..))
+import Data.Newtype (unwrap)
 import Perspectives.ComputedTripleGetters (parserMessagesM, syntacticStateM)
 import Perspectives.CoreTypes (MonadPerspectives)
 import Perspectives.DomeinCache (retrieveDomeinFileFromCache, storeDomeinFileInCouchdb)
-import Perspectives.EntiteitAndRDFAliases (ID)
+import Perspectives.Representation.InstanceIdentifiers (ContextInstance, Value(..))
 import Perspectives.RunMonadPerspectivesQuery ((##>>))
 
 -- | Store the DomeinFile, if found in the cache.
-storeDomeinFile :: ID -> MonadPerspectives Unit
+storeDomeinFile :: ContextInstance -> MonadPerspectives Unit
 storeDomeinFile textId = do
   syntacticState <- textId ##>> syntacticStateM
   case syntacticState of
-    "false" -> pure unit
+    Value "false" -> pure unit
     otherwise -> do
       contextId <- textId ##>> parserMessagesM
       -- This must be the model name.
       -- here, because the syntactic state is true, there must be a contextId in the cache.
-      mdf <- retrieveDomeinFileFromCache contextId
+      mdf <- retrieveDomeinFileFromCache $ unwrap contextId
       case mdf of
         Nothing -> pure unit
         (Just df) -> storeDomeinFileInCouchdb df
