3ab3491654297bc9e8002b4e42992a9be4bc4bc4
Joop Ringelberg <joop.ringelberg@gmail.com>
2019-08-29 11:18:26 +0200

    All instance objectGetters now track dependencies

--- src/core/instances/instanceObjectGetters.purs
+++ src/core/instances/instanceObjectGetters.purs
@@ -1,77 +1,124 @@
 module Perspectives.Instances.ObjectGetters where
 
 import Control.Monad.Writer (lift, tell)
-import Data.Array (findIndex, index, nub, singleton)
-import Data.Maybe (Maybe(..), fromJust, maybe)
+import Data.Array (findIndex, index)
+import Data.Maybe (Maybe(..), fromJust)
 import Data.Newtype (unwrap)
 import Data.String.Regex (test)
 import Data.String.Regex.Flags (noFlags)
 import Data.String.Regex.Unsafe (unsafeRegex)
-import Foreign.Object (keys, lookup, values)
+import Foreign.Object (keys, lookup)
 import Partial.Unsafe (unsafePartial)
-import Perspectives.ContextAndRole (context_buitenRol, context_iedereRolInContext, context_pspType, context_rolInContext, rol_binding, rol_context, rol_properties, rol_property, rol_pspType)
+import Perspectives.ContextAndRole (context_buitenRol, context_pspType, rol_binding, rol_context, rol_properties, rol_pspType)
 import Perspectives.ContextRolAccessors (getContextMember, getRolMember)
-import Perspectives.CoreTypes (MonadPerspectives, type (~~>), assumption, type (##>))
+import Perspectives.CoreTypes (type (~~>), assumption, type (##>))
 import Perspectives.DependencyTracking.Array.Trans (ArrayT(..))
 import Perspectives.Identifiers (LocalName)
-import Perspectives.InstanceRepresentation (PerspectContext, PerspectRol(..))
+import Perspectives.InstanceRepresentation (PerspectContext(..), PerspectRol(..))
 import Perspectives.Instances (getPerspectEntiteit)
 import Perspectives.Representation.InstanceIdentifiers (ContextInstance, RoleInstance, Value)
-import Perspectives.Representation.TypeIdentifiers (ContextType, EnumeratedPropertyType, EnumeratedRoleType(..))
-import Prelude (identity, join, ($), (<>), (>=>), (<<<), pure, (*>))
+import Perspectives.Representation.TypeIdentifiers (ContextType, EnumeratedPropertyType, EnumeratedRoleType)
+import Prelude (($), (<>), (<<<), pure, (*>), bind, discard)
 
+-----------------------------------------------------------
+-- FUNCTIONS FROM CONTEXT
+-----------------------------------------------------------
 trackContextDependency :: EnumeratedRoleType -> (ContextInstance ##> RoleInstance) -> (ContextInstance ~~> RoleInstance)
 trackContextDependency roleName f c = (lift $ tell [(assumption (unwrap c) (unwrap roleName))]) *> (ArrayT $ lift $ f c)
 
-buitenRol :: ContextInstance ##> RoleInstance
-buitenRol = (getContextMember \c -> [context_buitenRol c])
+-- | Because we never change the ExternalRole of a Context, we have no need
+-- | to track it as a dependency.
+buitenRol :: ContextInstance ~~> RoleInstance
+buitenRol = ArrayT <<< lift <<< (getContextMember \c -> [context_buitenRol c])
 
-buitenRol' :: ContextInstance ~~> RoleInstance
-buitenRol' = trackContextDependency (EnumeratedRoleType "model:Perspectives$Context$buitenRol") buitenRol
+getRole :: EnumeratedRoleType -> (ContextInstance ~~> RoleInstance)
+getRole rn c = ArrayT do
+  ((PerspectContext{rolInContext}) :: PerspectContext) <- lift $ getPerspectEntiteit c
+  case (lookup (unwrap rn) rolInContext) of
+    Nothing -> pure []
+    (Just r) -> do
+      tell [assumption (unwrap c)(unwrap rn)]
+      pure r
 
-context :: RoleInstance ##> ContextInstance
-context = getRolMember \rol -> [rol_context rol]
+-- getRole rn = trackContextDependency rn (getContextMember \(ctxt :: PerspectContext) -> (context_rolInContext ctxt rn))
 
-iedereRolInContext :: (ContextInstance ##> RoleInstance)
-iedereRolInContext = getContextMember \ctxt -> nub $ join $ values (context_iedereRolInContext ctxt)
+-- | Because we never change the type of a Context, we have no real need
+-- | to track it as a dependency.
+contextType :: ContextInstance ~~> ContextType
+contextType = ArrayT <<< lift <<< (getContextMember \c -> [context_pspType c])
 
-binding :: RoleInstance ##> RoleInstance
-binding = getRolMember \rol -> maybe [] singleton (rol_binding rol)
+-----------------------------------------------------------
+-- FUNCTIONS FROM ROLE
+-----------------------------------------------------------
+-- | The ability to retrieve the Context of a RoleInstance depends on that RoleInstance being a Role of that Context.
+context :: RoleInstance ~~> ContextInstance
+context rid = ArrayT do
+  (r :: PerspectRol) <- lift $ getPerspectEntiteit rid
+  tell [(assumption (unwrap $ rol_context r) (unwrap $ rol_pspType r))]
+  pure $ [rol_context r]
 
-getProperty :: EnumeratedPropertyType -> (RoleInstance ##> Value)
-getProperty pn = getRolMember \(rol :: PerspectRol) -> rol_property rol (unwrap pn)
+binding :: RoleInstance ~~> RoleInstance
+binding r = ArrayT do
+  (role :: PerspectRol) <- lift $ getPerspectEntiteit r
+  case rol_binding role of
+    Nothing -> pure []
+    (Just b) -> do
+      tell [assumption (unwrap r) "model:Perspectives$Role$binding"]
+      pure [b]
 
-getRole :: EnumeratedRoleType -> (ContextInstance ##> RoleInstance)
-getRole rn = getContextMember \(ctxt :: PerspectContext) -> (context_rolInContext ctxt rn)
+getProperty :: EnumeratedPropertyType -> (RoleInstance ~~> Value)
+getProperty pn r = ArrayT do
+  ((PerspectRol{properties}) :: PerspectRol) <- lift $ getPerspectEntiteit r
+  case (lookup (unwrap pn) properties) of
+    Nothing -> pure []
+    (Just p) -> do
+      tell [assumption (unwrap r)(unwrap pn)]
+      pure p
 
 -- | Get the values for the property with the local name that are directly represented on the instance of a rol of type r, including AspectProperties.
 -- | E.g. getUnqualifiedProperty "voornaam"
-getUnqualifiedProperty :: LocalName -> (RoleInstance ##> Value)
-getUnqualifiedProperty ln = getRolMember $ getUnQualifiedPropertyFromPerspectRol ln
+getUnqualifiedProperty :: LocalName -> (RoleInstance ~~> Value)
+getUnqualifiedProperty ln r = ArrayT do
+  (role@(PerspectRol{properties}) :: PerspectRol) <- lift $ getPerspectEntiteit r
+  case findIndex (test (unsafeRegex (ln <> "$") noFlags)) (keys properties) of
+    Nothing -> pure []
+    (Just i) -> do
+      pn <- pure (unsafePartial $ fromJust (index (keys $ rol_properties role) i))
+      case (lookup pn properties) of
+        Nothing -> pure []
+        (Just p) -> do
+          tell [assumption (unwrap r) pn]
+          pure p
 
-getUnQualifiedPropertyFromPerspectRol :: LocalName -> PerspectRol -> Array Value
-getUnQualifiedPropertyFromPerspectRol ln rol =
-  case findIndex (test (unsafeRegex (ln <> "$") noFlags)) (keys $ rol_properties rol) of
-    Nothing -> []
-    (Just i) -> maybe [] identity (lookup (unsafePartial $ fromJust (index (keys $ rol_properties rol) i)) (rol_properties rol))
+-- | Because we never change the type of a Role, we have no real need
+-- | to track it as a dependency.
+roleType :: RoleInstance ~~> EnumeratedRoleType
+roleType = ArrayT <<< lift <<< (getRolMember \r -> [rol_pspType r])
 
-contextType :: ContextInstance -> MonadPerspectives ContextType
-contextType = getPerspectEntiteit >=> pure <<< context_pspType
-
-roleType :: RoleInstance -> MonadPerspectives EnumeratedRoleType
-roleType = getPerspectEntiteit >=> pure <<< rol_pspType
-
--- | From the instance of a Rol of any kind, find the instances of the Rol of the given type that bind it (that have
--- | it as their binding). The type of rname (RolDef) can be a BuitenRol.
--- Test.Perspectives.ObjectGetterConstructors
-getRoleBinders :: EnumeratedRoleType -> (RoleInstance ##> RoleInstance)
-getRoleBinders rname = getRolMember \(PerspectRol{gevuldeRollen}) -> maybe [] identity (lookup (unwrap rname) gevuldeRollen)
+-- | From the instance of a Rol of any kind, find the instances of the Rol of the given
+-- | type that bind it (that have it as their binding). The type of rname (RolDef) may
+-- | be psp:Context$externalRole.
+getRoleBinders :: EnumeratedRoleType -> (RoleInstance ~~> RoleInstance)
+getRoleBinders rname r = ArrayT do
+  ((PerspectRol{gevuldeRollen}) :: PerspectRol) <- lift $ getPerspectEntiteit r
+  case (lookup (unwrap rname) gevuldeRollen) of
+    Nothing -> pure []
+    (Just g) -> do
+      tell [assumption (unwrap r) (unwrap rname)]
+      pure g
 
 -- | From the instance of a Rol of any kind, find the instances of the Rol with the given local name
--- | that bind it (that have it as their binding). The type of ln can be 'buitenRolBeschrijving'.
+-- | that bind it (that have it as their binding). The type of ln can be 'externalRole'.
 -- Test.Perspectives.ObjectGetterConstructors
-getUnqualifiedRoleBinders :: LocalName -> (RoleInstance ##> RoleInstance)
-getUnqualifiedRoleBinders ln = getRolMember \(PerspectRol{gevuldeRollen}) ->
+getUnqualifiedRoleBinders :: LocalName -> (RoleInstance ~~> RoleInstance)
+getUnqualifiedRoleBinders ln r = ArrayT do
+    (role@(PerspectRol{gevuldeRollen}) :: PerspectRol) <- lift $ getPerspectEntiteit r
     case findIndex (test (unsafeRegex (ln <> "$") noFlags)) (keys gevuldeRollen) of
-      Nothing -> []
-      (Just i) -> maybe [] identity (lookup (unsafePartial $ fromJust (index (keys gevuldeRollen) i)) gevuldeRollen)
+      Nothing -> pure []
+      (Just i) -> do
+        rn <- pure (unsafePartial $ fromJust (index (keys gevuldeRollen) i))
+        case lookup rn gevuldeRollen of
+          Nothing -> pure []
+          (Just b) -> do
+            tell [assumption (unwrap r) rn]
+            pure b
