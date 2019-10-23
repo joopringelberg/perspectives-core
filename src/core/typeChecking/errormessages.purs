b0761de0a79be43d5b27004e01825bc0ecfacc0c
Joop Ringelberg <joop.ringelberg@gmail.com>
2019-10-23 13:04:38 +0200

    License header added to all files

--- src/core/typeChecking/errormessages.purs
+++ src/core/typeChecking/errormessages.purs
@@ -1,3 +1,24 @@
+-- BEGIN LICENSE
+-- Perspectives Distributed Runtime
+-- Copyright (C) 2019 Joop Ringelberg (joopringelberg@perspect.it), Cor Baars
+--
+-- This program is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with this program.  If not, see <https://www.gnu.org/licenses/>.
+--
+-- Full text of this license can be found in the LICENSE file in the projects root.
+
+-- END LICENSE
+
 module Perspectives.Checking.PerspectivesTypeChecker.Messages where
 
 import Perspectives.EntiteitAndRDFAliases (ID, ContextID, RolName, PropertyName, RolID) as Alias
--- src/core/typerepresentation/abstractDataType.purs
+++ src/core/typerepresentation/abstractDataType.purs
@@ -6,6 +6,27 @@
 -- | `SUM x NOTYPE` equals `NOTYPE`
 -- | `PROD x NOTYPE` equals `x`
 
+-- BEGIN LICENSE
+-- Perspectives Distributed Runtime
+-- Copyright (C) 2019 Joop Ringelberg (joopringelberg@perspect.it), Cor Baars
+--
+-- This program is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with this program.  If not, see <https://www.gnu.org/licenses/>.
+--
+-- Full text of this license can be found in the LICENSE file in the projects root.
+
+-- END LICENSE
+
 module Perspectives.Representation.ADT where
 
 import Data.Array (elemIndex, filter, head, intersect, uncons, union)
