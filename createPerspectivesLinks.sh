b06c5e5d1e6d0987ba9af3827b06d281df58f164
Joop Ringelberg <joop.ringelberg@gmail.com>
2019-11-22 15:18:14 +0100

    Actualised createPerspectivesLinks.sh

--- createPerspectivesLinks.sh
+++ createPerspectivesLinks.sh
@@ -1,23 +1,29 @@
 #!/usr/bin/env bash
 
-# NOTE: this script should be adapted with each new tagged version of aff-sockets!
+# NOTE: this script should be adapted with each new tagged version!
 
-cd .psc-package/psc-12.t-perspectives/aff-sockets
+cd .psc-package/psc-12.5-perspectives/aff-sockets
 
-rm -Rf v1.1.1
+rm -Rf v2.0.0
 
-ln -s ../../../../purescript-aff-sockets/ v1.1.1
+ln -s ../../../../purescript-aff-sockets/ v2.0.0
 
 cd ../perpectives-apitypes
 
-rm -Rf v1.0.0
+rm -Rf v2.0.0
 
 ln -s ../../../../perspectives-apitypes v2.0.0
 
 cd ../avar-monadask/
 
-rm -Rf v1.0.0
+rm -Rf v2.0.0
 
-ln -s ../../../../avar-monadask v1.0.0
+ln -s ../../../../purescript-avar-monadask v2.0.0
+
+cd ../perspectives-couchdb/
+
+rm -Rf v2.5.0
+
+ln -s ../../../../perspectives-couchdb v2.4.0
 
 cd ../../..
