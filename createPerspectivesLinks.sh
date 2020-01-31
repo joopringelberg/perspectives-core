faf664b3872ec65a6a36d63518815d2a3772831c
Joop Ringelberg <joop.ringelberg@gmail.com>
2020-01-31 17:47:13 +0100

    Version v0.2.0

--- createPerspectivesLinks.sh
+++ createPerspectivesLinks.sh
@@ -2,7 +2,7 @@
 
 # NOTE: this script should be adapted with each new tagged version!
 
-cd .psc-package/psc-12.6-perspectives/aff-sockets
+cd .psc-package/psc-12.7-perspectives/aff-sockets
 
 rm -Rf v2.1.0
 
@@ -10,9 +10,9 @@ ln -s ../../../../purescript-aff-sockets/ v2.1.0
 
 cd ../perpectives-apitypes
 
-rm -Rf v2.1.0
+rm -Rf v2.2.0
 
-ln -s ../../../../perspectives-apitypes v2.1.0
+ln -s ../../../../perspectives-apitypes v2.2.0
 
 cd ../avar-monadask/
 
@@ -22,8 +22,8 @@ ln -s ../../../../purescript-avar-monadask v2.1.0
 
 cd ../perspectives-couchdb/
 
-rm -Rf v2.5.6
+rm -Rf v2.5.7
 
-ln -s ../../../../perspectives-couchdb v2.5.6
+ln -s ../../../../perspectives-couchdb v2.5.7
 
 cd ../../..
