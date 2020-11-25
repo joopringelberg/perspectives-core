919f59ad3eda4b53fd8049098e54a37923041e68
Joop Ringelberg <joop.ringelberg@gmail.com>
2020-11-25 11:22:45 +0100

    version v8.0.0

--- createPerspectivesLinks.sh
+++ createPerspectivesLinks.sh
@@ -2,7 +2,7 @@
 
 # NOTE: this script should be adapted with each new tagged version!
 
-cd .psc-package/pv0.7.1/aff-sockets
+cd .psc-package/pv0.8.0/aff-sockets
 
 rm -Rf v2.2.0
 
@@ -12,7 +12,7 @@ cd ../perpectives-apitypes
 
 rm -Rf v2.6.0
 
-ln -s ../../../../perspectives-apitypes v2.6.0
+ln -s ../../../../perspectives-apitypes v2.7.0
 
 cd ../avar-monadask/
 
