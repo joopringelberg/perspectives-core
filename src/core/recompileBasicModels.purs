62c431d7cd23d8e5583e8375e9943ee6a869598b
Joop Ringelberg <joop.ringelberg@gmail.com>
2021-11-10 10:53:01 +0100

    Forgot to check in RecompileBasicModels

--- src/core/recompileBasicModels.purs
+++ src/core/recompileBasicModels.purs
@@ -27,7 +27,8 @@ module Main.RecompileBasicModels where
 -- | using this account, all models can be recompiled from the client.
 import Prelude
 
-import Data.Array (elemIndex, filterA)
+import Control.Monad.Trans.Class (lift)
+import Data.Array (elemIndex, filterA, head)
 import Data.Either (Either(..))
 import Data.Foldable (for_)
 import Data.Maybe (Maybe(..), isJust)
@@ -36,19 +37,24 @@ import Effect (Effect)
 import Effect.Aff (Error, runAff, throwError, error)
 import Effect.Aff.AVar (new)
 import Foreign (Foreign)
-import Perspectives.CoreTypes (MonadPerspectives, (##=), (##>), (##>>))
+import Perspectives.CoreTypes (MonadPerspectives, (##=), (##>))
+import Perspectives.Couchdb.Revision (changeRevision)
+import Perspectives.DomeinFile (DomeinFile(..), DomeinFileId(..))
 import Perspectives.ErrorLogging (logPerspectivesError)
 import Perspectives.Extern.Couchdb (contextInstancesFromCouchdb, modelsDatabaseName, roleInstancesFromCouchdb)
-import Perspectives.Extern.Parsing (uploadToRepository)
-import Perspectives.Instances.ObjectGetters (binding, context, getEnumeratedRoleInstances, getProperty)
+import Perspectives.External.CoreModules (addAllExternalFunctions)
+import Perspectives.Instances.ObjectGetters (binding, context, getEnumeratedRoleInstances, getProperty, getPropertyFromTelescope)
 import Perspectives.Parsing.Messages (PerspectivesError(..))
+import Perspectives.Persistence.API (retrieveDocumentVersion)
 import Perspectives.Persistence.State (getCouchdbBaseURL)
-import Perspectives.Persistence.Types (Url, UserName, PouchdbUser, decodePouchdbUser')
+import Perspectives.Persistence.Types (Url, PouchdbUser, decodePouchdbUser')
+import Perspectives.Persistent (saveEntiteit_)
 import Perspectives.PerspectivesState (newPerspectivesState)
 import Perspectives.Representation.InstanceIdentifiers (RoleInstance(..), Value(..))
 import Perspectives.Representation.TypeIdentifiers (EnumeratedPropertyType(..), EnumeratedRoleType(..), RoleType(..))
 import Perspectives.RunMonadPerspectivesTransaction (runMonadPerspectivesTransaction')
 import Perspectives.RunPerspectives (runPerspectivesWithState)
+import Perspectives.TypePersistence.LoadArc (loadArcAndCrl')
 
 basicModels :: Array String
 basicModels = ["model:System", "model:ModelManagement"]
@@ -57,8 +63,10 @@ basicModels = ["model:System", "model:ModelManagement"]
 -- | has changed. The local models directory of the user that is provided, will have the freshly compiled
 -- | DomeinFiles, so this user can be booted. The essential models include model:ModelManagement, so that
 -- | using this account, all models can be recompiled from the client.
-recompileBasicModels :: UserName -> Foreign -> Url -> (Boolean -> Effect Unit) -> Effect Unit
-recompileBasicModels usr rawPouchdbUser publicRepo callback = void $ runAff handler
+-- | These functions depend heavily on the Perspectives types in model:System and
+-- | model:ModelManagement.
+recompileBasicModels :: Foreign -> Url -> (Boolean -> Effect Unit) -> Effect Unit
+recompileBasicModels rawPouchdbUser publicRepo callback = void $ runAff handler
   do
     case decodePouchdbUser' rawPouchdbUser of
       Left e -> throwError (error "Wrong format for parameter 'rawPouchdbUser' in resetAccount")
@@ -66,12 +74,12 @@ recompileBasicModels usr rawPouchdbUser publicRepo callback = void $ runAff hand
         state <- new $ newPerspectivesState pouchdbUser publicRepo
         runPerspectivesWithState
           (do
-            modelsDb <- modelsDatabaseName
-            mmodelManagementApp <- (RoleInstance "") ##> contextInstancesFromCouchdb ["model:ModelManagement$Model$ModelManagementApp"]
+            addAllExternalFunctions
+            mmodelManagementApp <- (RoleInstance "") ##> contextInstancesFromCouchdb ["model:ModelManagement$ModelManagementApp"]
             case mmodelManagementApp of
               Nothing -> logPerspectivesError $ Custom $ "Cannot find instance of ModelManagementApp!"
               Just modelManagementApp -> do
-                (managedModels :: Array RoleInstance) <- modelManagementApp ##= roleInstancesFromCouchdb ["model:ModelManagement$Model$ModelManagementApp$Models"]
+                (managedModels :: Array RoleInstance) <- modelManagementApp ##= roleInstancesFromCouchdb ["model:ModelManagement$ModelManagementApp$Models"]
                 modelsToCompile <- (filterA isBasicModel managedModels)
                 for_ modelsToCompile recompileModel
             )
@@ -82,24 +90,39 @@ recompileBasicModels usr rawPouchdbUser publicRepo callback = void $ runAff hand
       logPerspectivesError $ Custom $ "An error condition in recompileBasicModels: " <> (show e)
       callback false
     handler (Right e) = do
-      logPerspectivesError $ Custom $ "Basic models recompiled!" <> usr
+      logPerspectivesError $ Custom $ "Basic models recompiled!"
       callback true
 
     -- RoleInstance is of type ModelManagementApp$Models
     recompileModel :: RoleInstance -> MonadPerspectives Unit
     recompileModel rid = do
-      arcSource <- rid ##= binding >=> getProperty (EnumeratedPropertyType "model:ModelManagement$ManagedModel$External$ArcSource")
-      crlSource <- rid ##= binding >=> getProperty (EnumeratedPropertyType "model:ModelManagement$ManagedModel$External$CrlSource")
+      arcSources <- rid ##= binding >=> getProperty (EnumeratedPropertyType "model:ModelManagement$ManagedModel$External$ArcSource")
+      crlSources <- rid ##= binding >=> getProperty (EnumeratedPropertyType "model:ModelManagement$ManagedModel$External$CrlSource")
       mcouchbaseUrl <- getCouchdbBaseURL
-      case mcouchbaseUrl of
-        Nothing -> logPerspectivesError $ Custom $ "No CouchdbUrl found!"
-        Just couchdbUrl -> void $ runMonadPerspectivesTransaction'
+      case head arcSources, head crlSources, mcouchbaseUrl of
+        Nothing, _, _ -> logPerspectivesError $ Custom $ "No Arc Source found!"
+        _, Nothing, _ -> logPerspectivesError $ Custom $ "No Crl Source found!"
+        _, _, Nothing -> logPerspectivesError $ Custom $ "No CouchdbUrl found!"
+        Just arcSource, Just crlSource, Just couchdbUrl -> void $ runMonadPerspectivesTransaction'
           false
           (ENR $ EnumeratedRoleType "model:System$PerspectivesSystem$User")
-          (uploadToRepository (unwrap <$> arcSource) (unwrap <$> crlSource) [couchdbUrl] [(RoleInstance "")])
+          do
+            r <- lift $ lift $ loadArcAndCrl' (unwrap arcSource) (unwrap crlSource)
+            case r of
+              Left m -> logPerspectivesError $ Custom ("recompileModel: " <> show m)
+              Right df@(DomeinFile drf@{_id}) -> lift $ lift do
+                modDb <- modelsDatabaseName
+                mrev <- retrieveDocumentVersion modDb _id
+                void $ saveEntiteit_ (DomeinFileId _id) (changeRevision mrev df)
 
+    -- | RoleInstance is of type "model:ModelManagement$ModelManagementApp$Models"
     isBasicModel :: RoleInstance -> MonadPerspectives Boolean
-    isBasicModel rid = (rid ##>> context >=> getEnumeratedRoleInstances (EnumeratedRoleType "model:ModelManagement$ManagedModel$ModelDescription") >=> binding >=> getProperty (EnumeratedPropertyType "model:System$Model$ModelIdentification")) >>= isBasicModel'
+    isBasicModel rid = (rid ##>
+      binding >=>
+      context >=>
+      getEnumeratedRoleInstances (EnumeratedRoleType "model:ModelManagement$ManagedModel$ModelDescription") >=>
+      getPropertyFromTelescope (EnumeratedPropertyType "model:System$Model$External$ModelIdentification")) >>= isBasicModel'
       where
-        isBasicModel' :: Value -> MonadPerspectives Boolean
-        isBasicModel' (Value modelId) = pure $ isJust $ elemIndex modelId basicModels
+        isBasicModel' :: Maybe Value -> MonadPerspectives Boolean
+        isBasicModel' (Just (Value modelId)) = pure $ isJust $ elemIndex modelId basicModels
+        isBasicModel' Nothing = (logPerspectivesError $ Custom $ "No ModelIdentification found for '" <> unwrap rid <> "'.") *> pure false
