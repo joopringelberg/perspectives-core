ee6572273400614d1101c08419f6f0d6221a92d1
Joop Ringelberg <joop.ringelberg@gmail.com>
2023-09-22 11:28:22 +0200

    Create password for Couchdb

--- src/core/authentication/authenticate.purs
+++ src/core/authentication/authenticate.purs
@@ -22,10 +22,30 @@
 
 module Perspectives.Authenticate where
 
--- | Top level entry to message authentication. Sign a message. Under the hood this requires fetching
--- | the users private key.
+import Prelude
+
+import Crypto.Subtle.Constants.AES (aesCBC, l256)
+import Crypto.Subtle.Hash (sha1)
+import Crypto.Subtle.Key.Derive (deriveBits, pbkdf2)
+import Crypto.Subtle.Key.Generate (aes, generateKey)
+import Crypto.Subtle.Key.Import (ImportAlgorithm, importKey)
+import Crypto.Subtle.Key.Types as CryptoTypes
+import Data.Array (foldMap)
+import Data.ArrayBuffer.ArrayBuffer (byteLength)
+import Data.ArrayBuffer.Builder (PutM, putArrayBuffer, execPut, putUint32be)
+import Data.ArrayBuffer.Typed (buffer, toArray, whole)
+import Data.ArrayBuffer.Types (ArrayBuffer, Int8Array)
+import Data.Int (hexadecimal, toStringAs)
 import Data.Maybe (Maybe(..))
+import Data.String (length)
+import Data.UInt (fromInt)
+import Effect (Effect)
+import Effect.Aff (Aff)
+import Effect.Class (class MonadEffect, liftEffect)
 import Perspectives.Representation.InstanceIdentifiers (RoleInstance)
+import Unsafe.Coerce (unsafeCoerce)
+import Web.Encoding.TextEncoder (encode, new)
+
 
 -- | Top level entry to message authentication. Sign a message. Under the hood this requires fetching
 -- | the users private key and encrypting the message.
@@ -41,3 +61,43 @@ authenticate author message = Just message
 -- | Use the current authors key to decrypt the message. This must succeed; otherwise we have a logical error.
 selfAuthenticate :: String -> String
 selfAuthenticate message = message
+
+pbkdf2_import :: ImportAlgorithm
+pbkdf2_import = unsafeCoerce "PBKDF2"
+
+-- Create a password 
+type Password = String
+createPassword :: Password -> Aff String
+createPassword password = do
+  -- Generate a random salt
+  (key :: CryptoTypes.CryptoKey) <- generateKey (aes aesCBC l256) true [CryptoTypes.encrypt, CryptoTypes.decrypt]
+  (salt :: ArrayBuffer) <- CryptoTypes.exportKey CryptoTypes.raw key
+  -- Convert the password to an ArrayBuffer and import it into a CryptoKey.
+  (passwordData :: ArrayBuffer) <- liftEffect $ execPut $ putStringUtf8 password
+  (baseKey :: CryptoTypes.CryptoKey) <- importKey CryptoTypes.raw passwordData pbkdf2_import false [CryptoTypes.deriveBits]
+  -- Now derive a key from the password and the salt.
+  (derivedKey :: ArrayBuffer) <- deriveBits (pbkdf2 sha1 salt 10000) baseKey 8
+  -- Finally, convert to a hexadecimal string representation.
+  liftEffect $ arrayBufferToHexString derivedKey
+
+  where 
+
+    putStringUtf8 :: forall m. MonadEffect m => String -> PutM m Unit
+    putStringUtf8 s = do
+      textEncoder <- liftEffect new
+      let (stringbuf :: ArrayBuffer) = buffer $ encode s textEncoder
+      -- Put a 32-bit big-endian length for the utf8 string, in bytes.
+      putUint32be $ fromInt $ byteLength stringbuf
+      putArrayBuffer stringbuf
+
+  -- Function to convert an ArrayBuffer to a hexadecimal string
+    arrayBufferToHexString :: ArrayBuffer -> Effect String
+    arrayBufferToHexString buffer = do
+      (int8array :: Int8Array) <- whole buffer
+      arr <- toArray int8array
+      pure $ foldMap toHex arr 
+      where
+        toHex :: Int -> String
+        toHex int = case toStringAs hexadecimal int of
+          s | length s == 1 -> "0" <> s
+          s -> s
