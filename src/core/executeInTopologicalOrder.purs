616748f509803d0f9ec635e52d4b3aa39c01246e
Joop Ringelberg <joop.ringelberg@gmail.com>
2023-10-16 15:37:41 +0200

    Topological sorting fixed
    Self-reference by models broke the algorithm.
    Furthermore, dependencies that are not in the complete list of
    references are no longer a problem.

--- src/core/executeInTopologicalOrder.purs
+++ src/core/executeInTopologicalOrder.purs
@@ -3,9 +3,11 @@ module Perspectives.ExecuteInTopologicalOrder where
 import Prelude
 
 import Control.Monad.Error.Class (class MonadThrow, throwError)
+import Control.Monad.State (StateT, evalStateT, get)
 import Control.Monad.Writer (WriterT, lift, runWriterT, tell)
-import Data.Array (cons, difference, foldM, length, null)
-import Data.Tuple (Tuple(..), snd)
+import Data.Array (cons, difference, elemIndex, filter, foldM, length, null)
+import Data.Maybe (isJust)
+import Data.Tuple (Tuple(..))
 import Perspectives.Parsing.Messages (PerspectivesError(..), MultiplePerspectivesErrors)
 
 --------------------------------------------------------------------------------------------
@@ -25,34 +27,43 @@ executeInTopologicalOrder :: forall m item label. MonadThrow MultiplePerspective
   (item -> Array label) ->
   ToSort item ->
   (item -> m item) ->
-  m (Array item)
-executeInTopologicalOrder getLabel getDependencies toSort action = snd <$> runWriterT (executeInTopologicalOrder' toSort [])
+  m (Sorted item)
+executeInTopologicalOrder getLabel getDependencies toSort action = evalStateT (executeInTopologicalOrder' toSort []) (getLabel <$> toSort)
   where
     executeInTopologicalOrder' ::
       ToSort item ->
-      Sorted item->
-      WriterT (Skipped item) m (Sorted item)
+      Sorted item ->
+      (StateT (ToSort label) m) (Sorted item)
     executeInTopologicalOrder' toSort' sortedItems = do
-      Tuple sortedItems' skipped <- lift $ runWriterT (foldM executeInTopologicalOrder'' sortedItems toSort')
+      Tuple sortedItems' skipped <- runWriterT (foldM executeInTopologicalOrder'' sortedItems toSort')
       if null skipped
+        -- If no items are left to sort, we're done.
         then pure sortedItems'
         else if length sortedItems == length sortedItems'
+          -- But if our effort did not increase the number of sorted items (that is, sortedItems'), stop.
           then throwError [(Custom ("Cannot topologically sort these items (are you sure all relevant items are included?): " <> show (getLabel <$> skipped)))]
-          -- If not done, rinse and repeat!
+          -- Otherwise, rinse and repeat by trying to sort the skipped items!
           else executeInTopologicalOrder' skipped sortedItems'
 
     executeInTopologicalOrder'' ::
       Sorted item ->
       item ->
-      WriterT (Skipped item) m (Sorted item)
-    executeInTopologicalOrder'' sortedItems item = if zeroInDegrees
-      then lift $ action item >>= pure <<< flip cons sortedItems
-      else do
-        tell [item]
-        pure sortedItems
+      WriterT (Skipped item) (StateT (ToSort label) m) (Sorted item)
+    executeInTopologicalOrder'' sortedItems item = do
+      allItems <- get
+      if zeroInDegrees allItems
+        -- This item has no other dependencies than those that have been handled before, so perform the action on it and tack it onto the sorted items.
+        then lift $ lift $ action item >>= pure <<< flip cons sortedItems
+        -- Some of the dependencies of this item have not been handled before. Push it to the skipped items and return the original sorted items.
+        -- Notice that if an item has a dependency that IS NOT in the original set of items to sort, it will never be handled!
+        else do
+          tell [item]
+          pure sortedItems
       where
-        zeroInDegrees :: Boolean
-        zeroInDegrees = null $ (getDependencies item) `difference` (getLabel <$> sortedItems)
+        -- All of the dependencies are already sorted.
+        -- INSTEAD, make sure that the item is not dependent on items that have not yet been sorted!
+        zeroInDegrees :: Array label -> Boolean
+        zeroInDegrees allItems = null $ (filter (isJust <<< flip elemIndex allItems) (getDependencies item)) `difference` (getLabel <$> sortedItems)
 
 -- | Sort the items topologically, using a function that gets an items dependencies, depending on item equality.
 sortTopologically  :: forall m item. MonadThrow MultiplePerspectivesErrors m => Eq item => Show item =>
