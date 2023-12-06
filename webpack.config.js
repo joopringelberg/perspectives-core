1e685f3fc80b6898d00651e90199147f3e714f3d
Joop Ringelberg <joop.ringelberg@gmail.com>
2023-12-06 15:01:54 +0100

    Development and production build

--- webpack.config.js
+++ webpack.config.js
@@ -3,45 +3,49 @@
 
 const path = require("path");
 
-module.exports = {
-  entry: path.join(__dirname, "output/Main/index.js" ),
-  output: {
-    library: "perspectives-core",
-    libraryTarget: "umd",
-    filename: "perspectives-core.js",
-    path: path.join(__dirname, "dist")
-  },
-  watch: false,
-  mode: "development",
-  target: "webworker",
-  module: {
-    rules: []
-  },
-  externals: {
-    "perspectives-proxy": {
-      commonjs: 'perspectives-proxy',
-      commonjs2: 'perspectives-proxy',
-      amd: 'perspectives-proxy',
-      root: 'perspectives-proxy'
+module.exports = function(env)
+  {
+    const {target} = env;
+    return {
+    entry: path.join(__dirname, "output/Main/index.js" ),
+    output: {
+      library: "perspectives-core",
+      libraryTarget: "umd",
+      filename: "perspectives-core.js",
+      path: path.join(__dirname, "dist")
     },
-    // These are Affjax dependencies when running on node.
-    "xhr2-cookies": {
-      commonjs: "xhr2-cookies",
-      commonjs2: "xhr2-cookies",
-      amd: "xhr2-cookies",
-      root: "xhr2-cookies"
+    watch: false,
+    mode: env.target,
+    target: "webworker",
+    module: {
+      rules: []
     },
-    "url": {
-      commonjs: "url",
-      commonjs2: "url",
-      amd: "url",
-      root: "url"
-    },
-    "pouchdb-browser": {
-      commonjs: "pouchdb-browser",
-      commonjs2: "pouchdb-browser",
-      amd: "pouchdb-browser",
-      root: "pouchdb-browser"
+    externals: {
+      "perspectives-proxy": {
+        commonjs: 'perspectives-proxy',
+        commonjs2: 'perspectives-proxy',
+        amd: 'perspectives-proxy',
+        root: 'perspectives-proxy'
+      },
+      // These are Affjax dependencies when running on node.
+      "xhr2-cookies": {
+        commonjs: "xhr2-cookies",
+        commonjs2: "xhr2-cookies",
+        amd: "xhr2-cookies",
+        root: "xhr2-cookies"
+      },
+      "url": {
+        commonjs: "url",
+        commonjs2: "url",
+        amd: "url",
+        root: "url"
+      },
+      "pouchdb-browser": {
+        commonjs: "pouchdb-browser",
+        commonjs2: "pouchdb-browser",
+        amd: "pouchdb-browser",
+        root: "pouchdb-browser"
+      }
     }
   }
 };
