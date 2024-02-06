9e8490c220bc51355637345e0f3c4f4327bb2fd5
Joop Ringelberg <joop.ringelberg@gmail.com>
2024-02-06 15:07:42 +0100

    Boundary functions for the Files Library

--- src/core/computedValues/perspectivesFiles.purs
+++ src/core/computedValues/perspectivesFiles.purs
@@ -2,6 +2,7 @@ module Perspectives.Extern.Files where
 
 import Prelude
 
+import Control.Monad.Error.Class (try)
 import Control.Monad.Trans.Class (lift)
 import Data.Array (head, singleton)
 import Data.Either (Either(..))
@@ -14,6 +15,7 @@ import Data.Tuple (Tuple(..))
 import Foreign (Foreign)
 import Perspectives.CoreTypes (MonadPerspectivesQuery)
 import Perspectives.DependencyTracking.Array.Trans (ArrayT(..))
+import Perspectives.Error.Boundaries (handleExternalFunctionError)
 import Perspectives.External.HiddenFunctionCache (HiddenFunctionDescription)
 import Perspectives.Identifiers (typeUri2couchdbFilename)
 import Perspectives.Instances.Values (parsePerspectivesFile)
@@ -22,19 +24,22 @@ import Perspectives.Representation.ThreeValuedLogic (ThreeValuedLogic(..))
 import Unsafe.Coerce (unsafeCoerce)
 
 fileText :: Array String -> String -> MonadPerspectivesQuery String
-fileText fileInfo_ _ = ArrayT case head fileInfo_ of
-  Just v -> case parsePerspectivesFile v of
-    Left e -> pure []
-    Right {propertyType, roleFileName, database, mimeType} -> if isATextType mimeType
-      then case roleFileName, database of 
-        Just rid, Just db -> do
-          (ma :: Maybe Foreign) <- lift $ getAttachment db rid (typeUri2couchdbFilename $ unwrap propertyType)
-          case ma of
-            Nothing -> pure []
-            Just a -> (lift $ lift $ fromBlob a) >>= pure <<< singleton
-        _, _ -> pure []
-      else pure []
-  Nothing -> pure []
+fileText fileInfo_ _ = try
+  (ArrayT case head fileInfo_ of
+    Just v -> case parsePerspectivesFile v of
+      Left e -> pure []
+      Right {propertyType, roleFileName, database, mimeType} -> if isATextType mimeType
+        then case roleFileName, database of 
+          Just rid, Just db -> do
+            (ma :: Maybe Foreign) <- lift $ getAttachment db rid (typeUri2couchdbFilename $ unwrap propertyType)
+            case ma of
+              Nothing -> pure []
+              Just a -> (lift $ lift $ fromBlob a) >>= pure <<< singleton
+          _, _ -> pure []
+        else pure []
+    Nothing -> pure [])
+  >>= handleExternalFunctionError "model://perspectives.domains#Files$FileText"
+
   where
     isATextType :: String -> Boolean
     isATextType = isJust <<< match (unsafeRegex "^text/" noFlags)
