bcfcf53ecc87cbc0013bf0e2032df4b654302e65
Joop Ringelberg <joop.ringelberg@gmail.com>
2024-12-20 16:42:07 +0100

    ##>> for binding >=> context is irresponsible!

--- src/core/perspectivesAPI.purs
+++ src/core/perspectivesAPI.purs
@@ -508,9 +508,12 @@ dispatchOnRequest r@{request, subject, predicate, object, reactStateSetter, corr
       if kind == ContextRole
         then do
           void $ runMonadPerspectivesTransaction authoringRole do
-            contextToBeRemoved <- lift ((RoleInstance subject) ##>> binding >=> context)
-            scheduleContextRemoval (Just autorizedRole) contextToBeRemoved
-            scheduleRoleRemoval (RoleInstance subject)
+            mcontextToBeRemoved <- lift ((RoleInstance subject) ##> binding >=> context)
+            case mcontextToBeRemoved of
+              Nothing -> scheduleRoleRemoval (RoleInstance subject)
+              Just contextToBeRemoved -> do
+                scheduleContextRemoval (Just autorizedRole) contextToBeRemoved
+                scheduleRoleRemoval (RoleInstance subject)
           sendResponse (Result corrId []) setter
         else sendResponse (Error corrId ("Cannot remove a context from a non-context role kind: " <> show autorizedRole )) setter
     Api.DeleteRole -> do
